03ef21586c378e30dfb5198af2a7b74f218c4b93
merri zibert <mzibert@cnm.edu>
2016-04-27 14:53:50 -0600

    started persona up to attitudes and behaviors

--- public_html/epic/britt-hoye.php
+++ public_html/epic/britt-hoye.php
@@ -12,7 +12,7 @@
 		<p><strong>Profession</strong>Full time Physical Therapsit, Air Force Wife, Mom and Part Time Crossfit Coach.Britt is obsessed with all things fitness.  She wakes up at 4:00am so she can get her workout in before heading to work.  She has obtained several physical fitness certifications and insists on spending most of her spare time coaching Crossfit.  There is only one thing that interrupts Britt's fitness routine, and that is good local breweries with great craft beers.</p>
 		<p><strong>Technology</strong>Britt has a Dell laptop at home that she uses mainly for her online certification tests.  She really relies on her Samsung Galaxy S6 Edge to connect her to all things.  She is an avid Facebook user, and loves exploring new websites and apps that enhance her life and her passions.
 		</p>
-		<p><strong>Attitudes and Behaviors</strong>
+		<p><strong>Attitudes and Behaviors</strong>Britt 
 
 		</p>
 		<p><strong>Goals:</strong>
