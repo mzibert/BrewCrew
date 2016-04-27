3f8150dcf70d2d9a73133c74b01cc55c73a200a4
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-04-27 15:09:26 -0600

    Added file with base html for conceptual model

---
+++ public_html/epic/conceptual-model.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Conceptual Model</title>
+	</head>
+	<body>
+		<header>
+			<h1>Conceptual Model</h1>
+		</header>
+		<main>
+			<p>Conceptual Model goes here.</p>
+		</main>
+	</body>
+</html>
