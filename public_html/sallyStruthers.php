afafbe834d6201461df11e0fc8e5b1cbe6969198
Arlene Graham <agraham14@cnm.edu>
2016-04-27 12:33:16 -0600

    updated sallyStruthers.php, added php folder for classes .php to go into in the future

--- public_html/sallyStruthers.php
+++ public_html/sallyStruthers.php
@@ -13,15 +13,32 @@
 		<h2>Persona</h2>
 		<p><strong>Name:</strong> Sally Struthers </p>
 		<p><strong>Age:</strong> 68</p>
-		<p><strong>Profession:</strong> Retired actress and philatripist, Sally Struthers is a 'beer aficionado'.</p>
-		<p><strong>Technology:</strong> Sally travels a great deal both for pleasure and to make personal appearances in support of her charities.</p>
-		<p><strong>Attitudes and Behaviors:</strong> .</p>
+		<p><strong>Profession:</strong> Retired actress and philanthropist, Sally Struthers is a 'beer aficionado'.</p>
+		<p><strong>Technology:</strong> Sally travels a great deal both for pleasure and to make personal appearances in support of her charities.She gets a new iPhone with every new release and utilizes various tablets as new products are introduced. </p>
+		<p><strong>Attitudes and Behaviors:</strong> Sally enjoys using both online and downloadable apps for entertainment purposes. Because she is very social, Sally likes to find interesting places to eat, drink and meet locals every place she visits. Sally often uses Yelp but sometimes finds Yelp unreliable in terms of finding unique places to enjoy. Being a long time advocate of craft beers Sally plans to explore her love of micro-brews during her stay in New Mexico. She wants to try the 'Award Winning' brewerys but also wants to 'go off the beaten path' and try the lesser known brewery's. </p>
 
 		<strong>Goals:</strong>
 		<ul>
-			<li>#</li>
-			<li>#</li>
-			<li>#</li>
+			<li>Sally wants to enjoy her favorite kinds of beer without having to discover by trail and error which brewers offer the qualities of beer she enjoys.</li>
+			<li>Sally wants to find beers that are 'less hoppy' and clear if they are Pilsners and Stouts that are less heavy than European Stouts such as Guiness. </li>
+			<li>Sally needs clear information on where to find these particular brews.</li>
+			<li>Sally is very social and knowledgable about craft brews. She would love to interact both on and offline with like minded people. She also networks in this way.</li>
+			<li>Other than location, Sally would like to see information regarding both the brewery's and the individual brews. Such as:
+				<ul>
+					<li>
+						consumer rating of brewery and individual brews
+					</li>
+					<li>
+						beer stats such as IBU, ABV, type
+					</li>
+					<li>
+						awards won by brewry
+					</li>
+					<li>
+						information on brewry history, brew master and crafters
+					</li>
+				</ul>
+			</li>
 		</ul>
 
 		<h2>Use Case</h2>
