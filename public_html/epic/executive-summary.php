d81959a61ff2dc9b7ffab5b3474bc67661297064
McGaughey <therealmcgaughey@gmail.com>
2016-04-27 16:06:17 -0600

    Testing - first github collaboration

---
+++ public_html/epic/executive-summary.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Brew Crew Executive Summary</title>
+	</head>
+	<body>
+		<main>
+			<p>Brew Crew (working name) is a web app technology company launched in 2016 with a vision to change the way craft beer lovers discover new brews. For breweries, we offer the ability to promote what's on tap. For beer lovers, our technology
+
+
+
+				The mission of the Beer Compass web application is to connect lovers of craft beer with new breweries and beers, create flavor profiles for brews based on user tags, and
+			<p>What sets our app apart from others is </p>
+		</main>
+	</body>
+</html>
