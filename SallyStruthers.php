2e29d3d32308da54b6bf757bc3467df5039f5db2
Arlene Graham <agraham14@cnm.edu>
2016-04-27 10:54:00 -0600

    add public_html, css directories, started sallyStruthers.php Persona/UseCase but not finished

---
+++ SallyStruthers.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>DB Design Persona and Use Case</title>
+		<link rel="stylesheet" type="text/css" href="public_html/css/style.css"/>
+	</head>
+	<body>
+		<header><h1><em>DB Design Persona and Use Case</em></h1>
+
+
+		</header>
+
+		<h2>Persona</h2>
+		<p><strong>Name:</strong> Wes Siler </p>
+		<p><strong>Age:</strong> 34</p>
+		<p><strong>Profession:</strong> Wes is an internet blogger for IndefinitelyWild.com. He is a self describe 'Social
+			Expert'. Wes posts between 25 and 50 articles a week on IndefinitelyWild.com that average 50 words each. In
+			addition he posts close to 100 comments to related articles promoting his articles.</p>
+		<p><strong>Technology:</strong> Uses a Mac Pro Desktop while in the
+			office, a MacBook Pro laptop at home, and an iPhone 6 with an unlimited 4G data connection.</p>
+		<p><strong>Attitudes and Behaviors:</strong> Wes spends 40 hours a week working on and promoting his
+			articles. In his free time his looks for new ideas 'out in the world'. Wes is very in tune to
+			all things 'fandom' as anything related to fan sites are good sources for referral links. Wes
+			also contributes to articles on sites related to his on 'fandom'. Due to his attention to detail
+			and his advanced writing skills, Wes has developed an following on several Wikia sites.</p>
+
+		<strong>Goals:</strong>
+		<ul>
+			<li>To pursue his fandom of interest</li>
+			<li>Fandom site that attracts many users from many demographics</li>
+			<li>It should be attractive flexible and easy to use across devices</li>
+		</ul>
+
+		<h2>Use Case</h2>
+		<ul>
+			<li>Experienced bloggers and social media users like Wes use a variety of devices to access fandom site</li>
+			<li>Experienced bloggers and social media users like Wes need and expect clean and easily accessible reading
+				and
+				editing features
+			</li>
+			<li>Wes and users like him will have the site as a favorite or the native browser will auto-fill URL</li>
+			<li>Wes will click on the Sign In button in the top right corner
+				<ul>
+					<li>editorId</li>
+					<li>editorPassWord</li>
+					<li>editorName</li>
+				</ul>
+			</li>
+			<li>Wes will be able to click on the Chat feature to chat with people in his network</li>
+			<li>Wes will be able to see any notifications
+				<ul>
+					<li>comments left on his articles</li>
+					<li>replies to comments</li>
+					<li>incoming chat messages</li>
+				</ul>
+			</li>
+			<li>Wes will open the fandom site and click 'Edit' to make corrections or contribute data to an article
+				<ul>
+					<li>editorId</li>
+					<li>editsId</li>
+					<li>articleId</li>
+					<li>editsDate</li>
+					<li>editsContent</li>
+				</ul>
+			</li>
+			<li>Wes will be able to access other articles from links within the article he is reading/editing/commenting
+				<ul>
+					<li>articleId</li>
+					<li>editorId</li>
+					<li>articleContent</li>
+					<li>articleMedia</li>
+				</ul>
+			</li>
+			<li>Wes will also often click on the 'Talk' button to add comments about the article</li>
+			<li>Wes will add his own links to all comments he leaves</li>
+			<li>Wes will have his links in his profile</li>
+			<li>Other users will develop a familiarity of Wes from his many edits and comments</li>
+			<li>Other uses will be able to access Wes's links from his profile and his comments</li>
+			<li>Wes will update articles will new data as new episodes come available</li>
+			<li>Wes will also update articles and add comments as he attends fandom events</li>
+			<li>A list of recent updates and comments will be in a viewable panel attacted to every article</li>
+			<li>Wes will see recent contributions from other users and will be able to respond</li>
+			<li>Wes will be able to access basic editing features native to the fandom website</li>
+			<li>Wes can expect the site to load quickly due to low resolution images</li>
+			<li>Wes will be able to access parts of the article via the Contents section</li>
+			<li>Wes can edit individual Content sections in each article</li>
+		</ul>
+
+
+	</body>
+</html>
---
+++ public_html/sallyStruthers.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>DB Design Persona and Use Case</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	</head>
+	<body>
+		<header><h1><em>DB Design Persona and Use Case</em></h1>
+
+		</header>
+
+		<h2>Persona</h2>
+		<p><strong>Name:</strong> Sally Struthers </p>
+		<p><strong>Age:</strong> 68</p>
+		<p><strong>Profession:</strong> Retired actress and philatripist, Sally Struthers is a 'beer aficionado'.</p>
+		<p><strong>Technology:</strong> Sally travels a great deal both for pleasure and to make personal appearances in support of her charities.</p>
+		<p><strong>Attitudes and Behaviors:</strong> .</p>
+
+		<strong>Goals:</strong>
+		<ul>
+			<li>#</li>
+			<li>#</li>
+			<li>#</li>
+		</ul>
+
+		<h2>Use Case</h2>
+		<ul>
+			<li>#</li>
+			<li>#
+			</li>
+			<li>#</li>
+			<li>Classes will be listed here
+				<ul>
+					<li>#</li>
+					<li>#</li>
+					<li>#</li>
+				</ul>
+			</li>
+			<li>more use case senario </li>
+			<li>redo list items as needed
+				<ul>
+					<li>comments left on reviews</li>
+					<li>replies to comments</li>
+					<li>#</li>
+				</ul>
+			</li>
+			<li>More classes possible
+				<ul>
+					<li>#Id</li>
+					<li>#Id</li>
+					<li>#Id</li>
+					<li>#Id</li>
+					<li>#Id</li>
+				</ul>
+			</li>
+			<li>#
+				<ul>
+					<li>#Id</li>
+					<li>#Id</li>
+					<li>#Id</li>
+					<li>#Id</li>
+				</ul>
+			</li>
+			<li>Wes will also often click on the 'Talk' button to add comments about the article</li>
+			<li>Wes will add his own links to all comments he leaves</li>
+			<li>Wes will have his links in his profile</li>
+			<li>Other users will develop a familiarity of Wes from his many edits and comments</li>
+			<li>Other uses will be able to access Wes's links from his profile and his comments</li>
+			<li>Wes will update articles will new data as new episodes come available</li>
+			<li>Wes will also update articles and add comments as he attends fandom events</li>
+			<li>A list of recent updates and comments will be in a viewable panel attacted to every article</li>
+			<li>Wes will see recent contributions from other users and will be able to respond</li>
+			<li>Wes will be able to access basic editing features native to the fandom website</li>
+			<li>Wes can expect the site to load quickly due to low resolution images</li>
+			<li>Wes will be able to access parts of the article via the Contents section</li>
+			<li>Wes can edit individual Content sections in each article</li>
+		</ul>
+
+
+	</body>
+</html>
