53e9f6d369261a7cfc95ba2a24c3233d0feda7d1
merri zibert <mzibert@cnm.edu>
2016-04-27 15:33:48 -0600

    working on adding goals

--- public_html/epic/britt-hoye.php
+++ public_html/epic/britt-hoye.php
@@ -12,10 +12,13 @@
 		<p><strong>Profession</strong>Full time Physical Therapsit, Air Force Wife, Mom and Part Time Crossfit Coach.Britt is obsessed with all things fitness.  She wakes up at 4:00am so she can get her workout in before heading to work.  She has obtained several physical fitness certifications and insists on spending most of her spare time coaching Crossfit.  There is only one thing that interrupts Britt's fitness routine, and that is good local breweries with great craft beers.</p>
 		<p><strong>Technology</strong>Britt has a Dell laptop at home that she uses mainly for her online certification tests.  She really relies on her Samsung Galaxy S6 Edge to connect her to all things.  She is an avid Facebook user, and loves exploring new websites and apps that enhance her life and her passions.
 		</p>
-		<p><strong>Attitudes and Behaviors</strong>Britt 
-
+		<p><strong>Attitudes and Behaviors</strong>Britt is a self-proclaimed craft beer aficionado and local brewery critic.  She loves craft beers so much it is the one thing that she willingly gave up her six pack for.  She has also turned her love of all things local breweries into date night and family time outings.  When asked for her opinion on a local brewery Britt can quickly and easily spout off her likes and dislikes about the brewery itself and then a full detailed description of her favorite on tap beers.
 		</p>
 		<p><strong>Goals:</strong>
+			<ul>
+			<li>Because Britt is already so well versed in all of the local breweries and the normal inventory they offer, Britt would like to be notified when any of the local breweries change or update their on tap offerings. </li>
+			<li></li>
+		</ul>
 
 		</p>
 		
