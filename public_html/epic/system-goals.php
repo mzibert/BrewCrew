bb3b4f8d7406a8ab34441bef05fb912db6fd5618
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-04-27 13:39:56 -0600

    Added base html for system goals and user stories.

---
+++ public_html/epic/system-goals.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>System Goals</title>
+	</head>
+	<body>
+		<main>
+		<p>By using this site, the user will achieve........</p>
+		</main>
+	</body>
+</html>
