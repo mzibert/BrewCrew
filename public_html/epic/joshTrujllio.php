98121b1bb298272959ed3588352145f10a1840f1
Arlene Graham <agraham14@cnm.edu>
2016-04-27 17:48:27 -0600

    added joshTrujilllo.php but not finished yet also corrected spelling errors

---
+++ public_html/epic/joshTrujllio.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>DB Design Persona and Use Case</title>
+		<link rel="stylesheet" type="text/css" href="../css/style.css"/>
+	</head>
+	<body>
+		<header><h1><em>DB Design Persona</em></h1>
+
+		</header>
+
+		<h2>Persona</h2>
+		<p><strong>Name:</strong> Josh Trujillo </p>
+		<p><strong>Age:</strong> 55</p>
+		<p><strong>Profession:</strong> Rosh is obsessed with nuances of flavor and texture.  He's the ideal companion at a beer dinner because he can distinguish every ingredient, every spice, every ounce of sweat and love that goes into creating food and beverage masterpieces. Josh's intensely refined palate and opinionated nature make him a fiery, sometimes frustrated, but always brilliant Head Brewer. Josh manages the brewery team with an equal mixture of humor, intensity, and love. Josh never leaves the brewery, but somehow he manages to cook gourmet meals and tend to an extensive garden.</p>
+		<p><strong>Technology:</strong>Josh uses a Macbook Air with a 1.6GHz Processor, 256 GB Storage, and 4GB memory.
+		</p>
+		<p><strong>Attitudes and Behaviors:</strong> Josh is passionate about promoting the Marble Brewery. Either he or one of his managers updates the Marbles daily Tweet feed that run on MarbleBrewery.com. In addition, Josh makes sure the world knows about the Marble through Instragram, YouTube and many Brew Competitions. </p>
+
+		<strong>Goals:</strong>
+		<ul>
+			<li>Josh would like to delete and add new brews.</li>
+			<li>Josh would also like to update location/hour information. </li>
+			<li></li>
+			<li></li>
+			<li>
+				<ul>
+					<li>
+						consumer rating of brewery and individual brews
+					</li>
+					<li>
+						beer stats such as IBU, ABV, type
+					</li>
+					<li>
+						awards won by brewry
+					</li>
+					<li>
+
+					</li>
+				</ul>
+			</li>
+		</ul>
--- public_html/epic/sallyStruthers.php
+++ public_html/epic/sallyStruthers.php
@@ -15,14 +15,14 @@
 		<p><strong>Age:</strong> 68</p>
 		<p><strong>Profession:</strong> Retired actress and philanthropist, Sally Struthers is a 'beer aficionado'.</p>
 		<p><strong>Technology:</strong> Sally travels a great deal both for pleasure and to make personal appearances in support of her charities.She gets a new iPhone with every new release and utilizes various tablets as new products are introduced. </p>
-		<p><strong>Attitudes and Behaviors:</strong> Sally enjoys using both online and downloadable apps for entertainment purposes. Because she is very social, Sally likes to find interesting places to eat, drink and meet locals every place she visits. Sally often uses Yelp but sometimes finds Yelp unreliable in terms of finding unique places to enjoy. Being a long time advocate of craft beers Sally plans to explore her love of micro-brews during her stay in New Mexico. She wants to try the 'Award Winning' brewerys but also wants to 'go off the beaten path' and try the lesser known brewery's. </p>
+		<p><strong>Attitudes and Behaviors:</strong> Sally enjoys using both online and downloadable apps for entertainment purposes. Because she is very social, Sally likes to find interesting places to eat, drink and meet locals every place she visits. Sally often uses Yelp but sometimes finds Yelp unreliable in terms of finding unique places to enjoy. Being a long time advocate of craft beers Sally plans to explore her love of micro-brews during her stay in New Mexico. She wants to try the 'Award Winning' breweries but also wants to 'go off the beaten path' and try the lesser known brewery's. </p>
 
 		<strong>Goals:</strong>
 		<ul>
 			<li>Sally wants to enjoy her favorite kinds of beer without having to discover by trail and error which brewers offer the qualities of beer she enjoys.</li>
 			<li>Sally wants to find beers that are 'less hoppy' and clear if they are Pilsners and Stouts that are less heavy than European Stouts such as Guiness. </li>
 			<li>Sally needs clear information on where to find these particular brews.</li>
-			<li>Sally is very social and knowledgable about craft brews. She would love to interact both on and offline with like minded people. She also networks in this way.</li>
+			<li>Sally is very social and knowledgeable about craft brews. She would love to interact both on and offline with like minded people. She also networks in this way.</li>
 			<li>Other than location, Sally would like to see information regarding both the brewery's and the individual brews. Such as:
 				<ul>
 					<li>
@@ -32,54 +32,16 @@
 						beer stats such as IBU, ABV, type
 					</li>
 					<li>
-						awards won by brewry
+						awards won by brewery
 					</li>
 					<li>
-						information on brewry history, brew master and crafters
+						information on brewery history, brew master and crafters
 					</li>
 				</ul>
 			</li>
 		</ul>
 
-		<h2>Use Case</h2>
-		<ul>
-			<li>#</li>
-			<li>#
-			</li>
-			<li>#</li>
-			<li>Classes will be listed here
-				<ul>
-					<li>#</li>
-					<li>#</li>
-					<li>#</li>
-				</ul>
-			</li>
-			<li>more use case senario </li>
-			<li>redo list items as needed
-				<ul>
-					<li>comments left on reviews</li>
-					<li>replies to comments</li>
-					<li>#</li>
-				</ul>
-			</li>
-			<li>More classes possible
-				<ul>
-					<li>#Id</li>
-					<li>#Id</li>
-					<li>#Id</li>
-					<li>#Id</li>
-					<li>#Id</li>
-				</ul>
-			</li>
-			<li>#
-				<ul>
-					<li>#Id</li>
-					<li>#Id</li>
-					<li>#Id</li>
-					<li>#Id</li>
-				</ul>
-			</li>
-		</ul>
+
 
 
 	</body>
