f0b45df90eeac5bb72ce66927da96e142256b59b
McGaughey <therealmcgaughey@gmail.com>
2016-04-28 15:58:37 -0600

    Fixed punctuation

--- public_html/epic/joshTrujllio.php
+++ public_html/epic/joshTrujllio.php
@@ -14,10 +14,9 @@
 		<p><strong>Name:</strong> Josh Trujillo </p>
 		<p><strong>Age:</strong> 55</p>
 		<p><strong>Profession:</strong> Brewmaster</p>
-		<p><strong>Description:</strong> Josh is obsessed with the nuances of flavor and texture.  He's the ideal companion at a dinner or tasting, because he can distinguish every ingredient, every spice, every ounce of sweat and love that goes into creating food and beverage masterpieces. Josh's intensely refined palate and opinionated nature make him a fiery, sometimes frustrated, but always brilliant Head Brewer. Josh manages the brewery team with an equal mixture of humor, intensity, and love. He never leaves the brewery, but somehow he manages to cook gourmet meals and tend to an extensive garden.</p>
 		<p><strong>Technology:</strong> Josh uses a Macbook Air with a 1.6GHz Processor, 256GB Storage, and 4GB memory.
 		</p>
-		<p><strong>Attitudes and Behaviors:</strong> Josh is passionate about promoting Marble Brewery in Albuquerque. Either he or one of his managers updates Marble's daily Twitter feed that runs on MarbleBrewery.com. In addition, Josh makes sure the world knows about the Marble through Instragram, YouTube and many brew competitions. </p>
+		<p><strong>Attitudes and Behaviors:</strong> Josh is obsessed with the nuances of flavor and texture.  He's the ideal companion at a dinner or tasting, because he can distinguish every ingredient, every spice, every ounce of sweat and love that goes into creating food and beverage masterpieces. Josh's intensely refined palate and opinionated nature make him a fiery, sometimes frustrated, but always brilliant Head Brewer. Josh manages the brewery team with an equal mixture of humor, intensity, and love. He never leaves the brewery, but somehow he manages to cook gourmet meals and tend to an extensive garden. Josh is passionate about promoting Marble Brewery in Albuquerque. Either he or one of his managers updates Marble's daily Twitter feed that runs on MarbleBrewery.com. In addition, Josh makes sure the world knows about the Marble through Instragram, YouTube and many brew competitions. </p>
 
 		<strong>Goals:</strong>
 		<ul>
--- public_html/epic/sallyStruthers.php
+++ public_html/epic/sallyStruthers.php
@@ -13,9 +13,9 @@
 		<h2>Persona</h2>
 		<p><strong>Name:</strong> Sally Struthers </p>
 		<p><strong>Age:</strong> 68</p>
-		<p><strong>Profession:</strong> Retired actress and philanthropist, Sally Struthers is a 'beer aficionado'.</p>
-		<p><strong>Technology:</strong> Sally travels a great deal both for pleasure and to make personal appearances in support of her charities.She gets a new iPhone with every new release and utilizes various tablets as new products are introduced. </p>
-		<p><strong>Attitudes and Behaviors:</strong> Sally enjoys using both online and downloadable apps for entertainment purposes. Because she is very social, Sally likes to find interesting places to eat, drink and meet locals every place she visits. Sally often uses Yelp but sometimes finds Yelp unreliable in terms of finding unique places to enjoy. Being a long time advocate of craft beers Sally plans to explore her love of micro-brews during her stay in New Mexico. She wants to try the 'Award Winning' breweries but also wants to 'go off the beaten path' and try the lesser known breweries. </p>
+		<p><strong>Profession:</strong> Retired actress and philanthropist, Sally Struthers is a 'beer aficionado.'</p>
+		<p><strong>Technology:</strong> Sally travels a great deal both for pleasure and to make personal appearances in support of her charities. She gets a new iPhone with every new release, and utilizes various tablets as new products are introduced. </p>
+		<p><strong>Attitudes and Behaviors:</strong> Sally enjoys using both online and downloadable apps for entertainment purposes. Because she is very social, Sally likes to find interesting places to eat, drink and meet locals in every place she visits. Sally often uses Yelp, but sometimes finds Yelp unreliable in terms of finding unique places to enjoy. Being a long time advocate of craft beers, Sally plans to explore her love of micro-brews during her stay in New Mexico. She wants to try the award winning beers, but also wants to go 'off the beaten path' and try lesser known brews. </p>
 
 		<strong>Goals:</strong>
 		<ul>
