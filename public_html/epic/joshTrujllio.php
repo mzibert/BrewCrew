24661680a70889b9f5142eef5d239f5308365fae
McGaughey <therealmcgaughey@gmail.com>
2016-04-28 15:52:58 -0600

    Final edits, just proofreading

--- public_html/epic/joshTrujllio.php
+++ public_html/epic/joshTrujllio.php
@@ -14,15 +14,13 @@
 		<p><strong>Name:</strong> Josh Trujillo </p>
 		<p><strong>Age:</strong> 55</p>
 		<p><strong>Profession:</strong> Brewmaster</p>
-		<p><strong>Description:</strong>
-		<p>Josh is obsessed with nuances of flavor and texture.  He's the ideal companion at a beer dinner because he can distinguish every ingredient, every spice, every ounce of sweat and love that goes into creating food and beverage masterpieces. Josh's intensely refined palate and opinionated nature make him a fiery, sometimes frustrated, but always brilliant Head Brewer. Josh manages the brewery team with an equal mixture of humor, intensity, and love. He never leaves the brewery, but somehow he manages to cook gourmet meals and tend to an extensive garden.</p>
-		<p><strong>Technology:</strong> Josh uses a Macbook Air with a 1.6GHz Processor, 256 GB Storage, and 4GB memory.
+		<p><strong>Description:</strong> Josh is obsessed with the nuances of flavor and texture.  He's the ideal companion at a dinner or tasting, because he can distinguish every ingredient, every spice, every ounce of sweat and love that goes into creating food and beverage masterpieces. Josh's intensely refined palate and opinionated nature make him a fiery, sometimes frustrated, but always brilliant Head Brewer. Josh manages the brewery team with an equal mixture of humor, intensity, and love. He never leaves the brewery, but somehow he manages to cook gourmet meals and tend to an extensive garden.</p>
+		<p><strong>Technology:</strong> Josh uses a Macbook Air with a 1.6GHz Processor, 256GB Storage, and 4GB memory.
 		</p>
-		<p><strong>Attitudes and Behaviors:</strong> Josh is passionate about promoting the Marble Brewery. Either he or one of his managers updates the Marbles daily Tweet feed that run on MarbleBrewery.com. In addition, Josh makes sure the world knows about the Marble through Instragram, YouTube and many Brew Competitions. </p>
+		<p><strong>Attitudes and Behaviors:</strong> Josh is passionate about promoting Marble Brewery in Albuquerque. Either he or one of his managers updates Marble's daily Twitter feed that runs on MarbleBrewery.com. In addition, Josh makes sure the world knows about the Marble through Instragram, YouTube and many brew competitions. </p>
 
 		<strong>Goals:</strong>
 		<ul>
-			<li></li>
 			<li>
 				Administrator hides out of season beers.
 			</li>
@@ -30,7 +28,7 @@
 				<ul>
 
 					<li>
-						Review beer
+						Rate and review beer
 					</li>
 					<li>
 						Tag beer
