b3cf7ff91e0d19a1f320ba8da093ee6eb091c5fd
McGaughey <therealmcgaughey@gmail.com>
2016-04-28 15:41:19 -0600

    Minor edits like correcting Rosh's name to Josh

--- public_html/epic/joshTrujllio.php
+++ public_html/epic/joshTrujllio.php
@@ -13,7 +13,8 @@
 		<h2>Persona</h2>
 		<p><strong>Name:</strong> Josh Trujillo </p>
 		<p><strong>Age:</strong> 55</p>
-		<p><strong>Profession:</strong> Rosh is obsessed with nuances of flavor and texture.  He's the ideal companion at a beer dinner because he can distinguish every ingredient, every spice, every ounce of sweat and love that goes into creating food and beverage masterpieces. Josh's intensely refined palate and opinionated nature make him a fiery, sometimes frustrated, but always brilliant Head Brewer. Josh manages the brewery team with an equal mixture of humor, intensity, and love. Josh never leaves the brewery, but somehow he manages to cook gourmet meals and tend to an extensive garden.</p>
+		<p><strong>Profession:</strong> Brewmaster</p>
+		<p>Josh is obsessed with nuances of flavor and texture.  He's the ideal companion at a beer dinner because he can distinguish every ingredient, every spice, every ounce of sweat and love that goes into creating food and beverage masterpieces. Josh's intensely refined palate and opinionated nature make him a fiery, sometimes frustrated, but always brilliant Head Brewer. Josh manages the brewery team with an equal mixture of humor, intensity, and love. He never leaves the brewery, but somehow he manages to cook gourmet meals and tend to an extensive garden.</p>
 		<p><strong>Technology:</strong>Josh uses a Macbook Air with a 1.6GHz Processor, 256 GB Storage, and 4GB memory.
 		</p>
 		<p><strong>Attitudes and Behaviors:</strong> Josh is passionate about promoting the Marble Brewery. Either he or one of his managers updates the Marbles daily Tweet feed that run on MarbleBrewery.com. In addition, Josh makes sure the world knows about the Marble through Instragram, YouTube and many Brew Competitions. </p>
