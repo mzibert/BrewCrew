7840ffa194f351f751ea072228282f54e01bb93c
McGaughey <therealmcgaughey@gmail.com>
2016-04-27 21:53:21 -0600

    Testing - first github collaboration

--- public_html/epic/executive-summary.php
+++ public_html/epic/executive-summary.php
@@ -6,12 +6,7 @@
 	</head>
 	<body>
 		<main>
-			<p>Brew Crew (working name) is a web app technology company launched in 2016 with a vision to change the way craft beer lovers discover new brews. For breweries, we offer the ability to promote what's on tap. For beer lovers, our technology lets you develop a flavor profile, get recomendations, rate, and review.
-
-
-
-				The mission of the Beer Compass web application is to connect lovers of craft beer with new breweries and beers, create flavor profiles for brews based on user tags, and
-			<p>What sets our app apart from others is </p>
+			<p>Brew Crew (working name) wants to change the way craft beer lovers discover new brews. For breweries, we offer the ability to promote what's on tap. For beer lovers, our technology helps you develop a flavor profile, get recommendations, rate, and review beers. What sets our app apart from others is a compass feature that maps the flavor and intensity of each craft brew in town, sharing with you the best in local beer. Cheers, Albuquerque!</p>
 		</main>
 	</body>
 </html>
