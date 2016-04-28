ba8f52517b84c04fded9510205fa96e4e83492b8
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-04-28 16:48:13 -0600

    Updated conceptual model to include full list of entities/attributes, and list of relations

--- public_html/epic/conceptual-model.php
+++ public_html/epic/conceptual-model.php
@@ -8,7 +8,102 @@
 		<header>
 			<h1>Conceptual Model</h1>
 		</header>
+		<main>
+			<h2>Attributes and Entities</h2>
+			<img src="images/brewCrewConceptualDiagram.svg" alt="Attributes and Entities in Image Format">
+			<!--Full list of attributes and entities-->
+			<h4>User</h4>
+			<ul>
+				<li>userId</li>
+				<li>breweryId</li>
+				<li>accessLevel</li>
+				<li>dateOfBirth</li>
+				<li>firstName</li>
+				<li>lastName</li>
+				<li>userEmail</li>
+				<li>username</li>
+				<li>userHash</li>
+				<li>userSalt</li>
+			</ul>
+			<h4>Brewery</h4>
+			<ul>
+				<li>breweryId</li>
+				<li>breweryDescription</li>
+				<li>breweryEstDate</li>
+				<li>breweryHours</li>
+				<li>breweryLocation</li>
+				<li>breweryPhone</li>
+				<li>breweryName</li>
+				<li>breweryURL</li>
+			</ul>
+			<h4>Beer</h4>
+			<ul>
+				<li>beerId</li>
+				<li>breweryId</li>
+				<li>tagId</li>
+				<li>beerABV</li>
+				<li>beerAvailability</li>
+				<li>beerAwards</li>
+				<li>beerColor</li>
+				<li>beerDescription</li>
+				<li>beerIBU</li>
+				<li>beerName</li>
+				<li>beerStyle</li>
+			</ul>
+			<h4>Review</h4>
+			<ul>
+				<li>reviewId</li>
+				<li>beerId</li>
+				<li>userId</li>
+				<li>tagId</li>
+				<li>pintRating</li>
+				<li>reviewDate</li>
+				<li>reviewText</li>
+			</ul>
+			<h4>Tag</h4>
+			<ul>
+				<li>tagId</li>
+				<li>beerId</li>
+				<li>tagLabel</li>
+			</ul>
 
-			<img src="images/brewCrewConceptualDiagram.svg">
+			<h2>Relations</h2>
+			<!--Relations in all forms formatted into a table-->
+			<table>
+				<tr>
+					<th>Verbal</th>
+					<th>Algebraic</th>
+					<th>Description</th>
+				</tr>
+				<tr>
+					<td>user-to-review</td>
+					<td>1-to-<i>n</i></td>
+					<td>Users can create many reviews/ratings (for different beers).</td>
+				</tr>
+				<tr>
+					<td>brewery-to-beer</td>
+					<td>1-to-<i>n</i></td>
+					<td>Breweries can have many associated beers.</td>
+				</tr>
+				<tr>
+					<td>beer-to-review</td>
+					<td>1-to-<i>n</i></td>
+					<td>Beers can have many reviews.</td>
+				</tr>
+				<tr>
+					<td>beer-to-tag</td>
+					<td>1-to-<i>n</i></td>
+					<td>Beers can possess many tags</td>
+				</tr>
+				<tr>
+					<td>review-to-tag</td>
+					<td>1-to-<i>n</i></td>
+					<td>Reviews can possess many tags.</td>
+				</tr>
+			</table>
+			<br>
+			<h2>ERD</h2>
+			<img src="images/brew-crew-erd.svg" alt="ERD for Beer Compass Project">
+		</main>
 	</body>
 </html>
