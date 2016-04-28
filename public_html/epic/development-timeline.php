098b437bba630ab9854191c1cd3d90e5b59266df
merri zibert <mzibert@cnm.edu>
2016-04-28 16:56:00 -0600

    finished development timeline

--- public_html/epic/development-timeline.php
+++ public_html/epic/development-timeline.php
@@ -1,10 +1,28 @@
 <!DOCTYPE html>
 <html lang="en"
-		<head>
-			<meta charset="utf-8">
-			<title>Development Timeline</title>
-		</head>
-		<body>
-			<h1>Development Timeline</h1>
-			
-		</body>
+<head>
+	<meta charset="utf-8">
+	<title>Development Timeline</title>
+</head>
+<body>
+	<h1>Development Timeline</h1>
+	<h2>Model</h2>
+	<!--All tasks needed to complete the back end-->
+	<ul>Task</ul>
+	<li>Code SQL Tables <strong>Due Date: May 6th, 2016</strong></li>
+	<li>Code Classes <strong>Due Date: May 6th, 2016</strong></li>
+	<li>Constructor <strong>Due Date: May 6th, 2016</strong></li>
+	<li>Testing<strong>Due Date: May 6th, 2016</strong></li>
+	<!--All tasks needed to complete the controller-->
+	<h2>Controller</h2>
+	<Ul>Task</Ul>
+	<li>Incorporate external API's <strong>Due Date: May 20th, 2016</strong></li>
+	<li>Testing <strong>Due Date: May 6th, 2016</strong></li>
+	<!--All tasks needed to complete the View-->
+	<h2>View</h2>
+	<ul>Task</ul>
+	<li>Site Map<strong>Due Date: June 3rd</strong></li>
+	<li>Wire Frames<strong>Due Date: June 3rd</strong></li>
+	<li>CSS Grid in Html<strong>Due Date: June 3rd</strong></li>
+	<li>testing<strong>Due Date: June 3rd</strong></li>
+</body>
