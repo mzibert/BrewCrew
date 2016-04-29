71b92fd51d141569e747ef8f6ce4cdc2b940867e
merri zibert <mzibert@cnm.edu>
2016-04-28 21:21:05 -0600

    fixed some errors in the development timeline

--- public_html/epic/development-timeline.php
+++ public_html/epic/development-timeline.php
@@ -9,24 +9,24 @@
 	<h2>Model</h2>
 	<!--All tasks needed to complete the back end-->
 	<ul>Task</ul>
-	<li>Code SQL Tables <strong>Due Date: May 6th, 2016</strong></li>
-	<li>Code Classes <strong>Due Date: May 6th, 2016</strong></li>
-	<li>Constructor <strong>Due Date: May 6th, 2016</strong></li>
-	<li>Testing<strong>Due Date: May 6th, 2016</strong></li>
+	<li>Code SQL Tables <strong> Due Date: May 6th, 2016</strong></li>
+	<li>Code Classes <strong> Due Date: May 6th, 2016</strong></li>
+	<li>Constructor <strong> Due Date: May 6th, 2016</strong></li>
+	<li>Testing<strong> Due Date: May 6th, 2016</strong></li>
 	<!--All tasks needed to complete the controller-->
 	<h2>Controller</h2>
 	<Ul>Task</Ul>
-	<li>Incorporate external API's <strong>Due Date: May 20th, 2016</strong></li>
-	<li>Testing <strong>Due Date: May 6th, 2016</strong></li>
+	<li>Incorporate external API's <strong> Due Date: May 20th, 2016</strong></li>
+	<li>Testing <strong> Due Date: May 20th, 2016</strong></li>
 	<!--All tasks needed to complete the View-->
 	<h2>View</h2>
 	<ul>Task</ul>
-	<li>Site Map<strong>Due Date: June 3rd</strong></li>
-	<li>Wire Frames<strong>Due Date: June 3rd</strong></li>
-	<li>CSS Grid in Html<strong>Due Date: June 3rd</strong></li>
-	<li>testing<strong>Due Date: June 3rd</strong></li>
+	<li>Site Map<strong> Due Date: June 3rd</strong></li>
+	<li>Wire Frames<strong> Due Date: June 3rd</strong></li>
+	<li>CSS Grid in Html<strong> Due Date: June 3rd</strong></li>
+	<li>testing<strong> Due Date: June 3rd</strong></li>
 	<!--Finishing touches and branding-->
 	<ul>/Branding</ul>
-	<li>Branding <strong>Due Date: June, 14th</strong></li>
+	<li>Branding <strong> Due Date: June, 14th</strong></li>
 	<li>Presentaion <strong> Due Date: June 15th</strong></li>
 </body>
