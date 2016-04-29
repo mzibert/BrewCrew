4b15c488a67949c93ef715ab3db1894fe38f2dc2
McGaughey <therealmcgaughey@gmail.com>
2016-04-29 09:58:37 -0600

    Added review table

--- brew-crew.sql
+++ brew-crew.sql
@@ -1,25 +1,19 @@
-CREATE TABLE Brewery (
-breweryId INT UNSIGNED AUTO_INCREMENT,
-breweryDescription VARCHAR(1000),
-breweryEstDate VARCHAR(150),
-breweryHours VARCHAR(250),
-breweryPhone VARCHAR(14),
-breweryName VARCHAR(100) NOT NULL,
-breweryUrl VARCHAR(100),
-PRIMARY KEY(breweryId)
+CREATE TABLE brewery (
+   breweryId INT UNSIGNED AUTO_INCREMENT,
+   breweryDescription VARCHAR(1000),
+   breweryEstDate VARCHAR(150),
+   breweryHours VARCHAR(250),
+   breweryPhone VARCHAR(14),
+   breweryName VARCHAR(100) NOT NULL,
+   breweryUrl VARCHAR(100),
+   PRIMARY KEY(breweryId)
 );
-CREATE TABLE  User  (
-   userId  INT UNSIGNED AUTO_INCREMENT NOT NULL,
-   userBreweryId  INT UNSIGNED AUTO_INCREMENT NOT NULL,
-   userAccessLevel  INT UNSIGNED AUTO_INCREMENT NOT NULL,
-   userActivationToken  INT UNSIGNED AUTO_INCREMENT NOT NULL,
-   userDateOfBirth  DATE NOT NULL,
-   userFirstName  VARCHAR(32) NOT NULL,
-   userLastName  VARCHAR(32) NOT NULL,
-   userEmail  VARCHAR(128) NOT NULL,
-   username  VARCHAR(32) NOT NULL,
-   userHash  <type>,
-   userSalt  <type>,
-  UNIQUE(username),
-  UNIQUE(userEmail),
-);
+
+CREATE TABLE review (
+   reviewId INT UNSIGNED AUTO_INCREMENT,
+   beerId INT UNSIGNED NOT NULL,
+   reviewUserId INT UNSIGNED NOT NULL,
+   reviewPintRating INT UNSIGNED NOT NULL,
+   reviewDate TIMESTAMP,
+   reviewText VARCHAR(2000),
+)
