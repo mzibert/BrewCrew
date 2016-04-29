1f12bcc980c6c6f33b1933aa112a950011186b49
Arlene Graham <agraham14@cnm.edu>
2016-04-29 09:56:36 -0600

    added User to mySQL

--- brew-crew.sql
+++ brew-crew.sql
@@ -8,3 +8,18 @@ breweryName VARCHAR(100) NOT NULL,
 breweryUrl VARCHAR(100),
 PRIMARY KEY(breweryId)
 );
+CREATE TABLE  User  (
+   userId  INT UNSIGNED AUTO_INCREMENT NOT NULL,
+   userBreweryId  INT UNSIGNED AUTO_INCREMENT NOT NULL,
+   userAccessLevel  INT UNSIGNED AUTO_INCREMENT NOT NULL,
+   userActivationToken  INT UNSIGNED AUTO_INCREMENT NOT NULL,
+   userDateOfBirth  DATE NOT NULL,
+   userFirstName  VARCHAR(32) NOT NULL,
+   userLastName  VARCHAR(32) NOT NULL,
+   userEmail  VARCHAR(128) NOT NULL,
+   username  VARCHAR(32) NOT NULL,
+   userHash  <type>,
+   userSalt  <type>,
+  UNIQUE(username),
+  UNIQUE(userEmail),
+);
