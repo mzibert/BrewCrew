bfa747d8a253d160f9aec6eda4f1b7feb5d57ad6
McGaughey <therealmcgaughey@gmail.com>
2016-04-29 09:45:34 -0600

    Renamed sql file

--- brew-crew.sql
+++ brew-crew.sql
@@ -1,9 +1,10 @@
 CREATE TABLE Brewery (
-breweryId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+breweryId INT UNSIGNED AUTO_INCREMENT,
 breweryDescription VARCHAR(1000),
 breweryEstDate VARCHAR(150),
 breweryHours VARCHAR(250),
 breweryPhone VARCHAR(14),
 breweryName VARCHAR(100) NOT NULL,
 breweryUrl VARCHAR(100),
-)
+PRIMARY KEY(breweryId)
+);
