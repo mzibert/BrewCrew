d753ead417aca0d5965c0e2df19c4475edf45b56
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-05-02 16:21:36 -0600

    Started on work for review class
    https://app.asana.com/0/117435671931079/118830311351353

--- public_html/Review.php
+++ public_html/Review.php
@@ -1,7 +1,7 @@
 <?php
 namespace Edu\Cnm\BREWCREW;
 
-require_once("autoload.php");
+require_once("autoload . php");
 
 /**
  * Review Class
@@ -23,29 +23,69 @@ class Review implements \JsonSerializable {
 	/**
 	 * reviewBeerId (aka beerId); a foreign key
 	 * @var int $reviewBeerId
-	 */
+	 **/
 	private $reviewBeerId;
 	/**
 	 * reviewUserId (aka userId); a foreign key
 	 * @var int $reviewUserId
-	 */
+	 **/
 	private $reviewUserId;
-	/**
-	 * reviewPintRating, the assigned rating for a beer on a 5-point scale
-	 * @var int $reviewPintRating
-	 */
-	private $reviewPintRating;
 	/**
 	 * reviewDate, the associated timestamp for a review
 	 * @var \DateTime $reviewDate
-	 */
+	 **/
 	private $reviewDate;
+	/**
+	 * reviewPintRating, the assigned rating for a beer on a 5-point scale
+	 * @var int $reviewPintRating
+	 **/
+	private $reviewPintRating;
 	/**
 	 * reviewText, text content for the review
 	 * @var string $reviewText
-	 */
+	 **/
 	private $reviewText;
 	
+	//CONSTRUCT goes here
+	//CONSTRUCT
+	//CONSTRUCT
+
+	
+	/**
+	 * accessor method for review id
+	 *
+	 * @return int|null value of review id, will be null if this is a new review
+	 **/
+	public function getReviewId() {
+		return($this->reviewId);
+	}
+
+	/**
+	 * mutator method for review id
+	 *
+	 * @param int|null $newReviewId creates new value for review id
+	 * @throws \RangeException if $newReviewId is not positive
+	 * @throws \TypeError if $newReviewId is not an integer
+	 **/
+	public function setReviewId(int $newReviewId = null) {
+		//when this is null, this is a new review with no mySQL id yet
+		if($newReviewId === null) {
+			$this->reviewId = null;
+			return;
+		}
+
+		//verify that the review id is positive
+		if($newReviewId <= 0) {
+			throw(new \RangeException("Review Id is not positive"));
+		}
+
+		//store the review id
+		$this->reviewId = $newReviewId;
+	}
+
+	/**
+	 * accessor method for
+	 */
 
 }
 
