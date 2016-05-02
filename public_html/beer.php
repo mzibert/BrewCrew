5e03b07c32bdf0414ece10993b4a909922c0f52d
merri zibert <mzibert@cnm.edu>
2016-05-02 15:15:39 -0600

    finished doc block for beer class https://app.asana.com/0/117423711360981/118830311351351

--- public_html/beer.php
+++ public_html/beer.php
@@ -7,21 +7,41 @@ class beer {
 	/** Id for this beer is assigned by the system; this is the primary key.
 	 * @var int $beerId
 	 **/
-	private $beerId
+	private $beerId;
 	/** Id of the brewery that has this beer; this is the foreign key
 	 * @var int $beerBreweryId
 	 **/
-	private $beerBreweryId
+	private $beerBreweryId;
 	/** Actual percentage measurement for value of alcohol by volume
 	 * @var float $beerAbv
 	 **/
-	private $beerAbv
+	private $beerAbv;
 	/** text field provided by the API to inform if and when beer is available
 	 * @var string $beerAvailability
 	 **/
-	private $beerAvailability
+	private $beerAvailability;
 	/** text field provided by the API to reflect any awards the beer has earned
 	 * @var string $beerAwards
 	 **/
-	private $beerAwards
+	private $beerAwards;
+	/** Decimal number created on a scale from 0 to 1 to reflect beer color on a scale from 0 being darkest to 1 being lightest
+	 * @var float $beerColor
+	 **/
+	private $beerColor;
+	/** string of open text field taken from the API used to describe the beer
+	 * @var string $beerDescription
+	 **/
+	private $beerDescription;
+	/** Integer used to evaluate the quantitative value designated to the measurement of bitterness in beer.  Lower values less bitter, higher values more bitter.
+	 * @var int $beerIbu
+	 **/
+	private $beerIbu;
+	/** string of open text field for the name of the beer
+	 * @var string $beerName
+	 **/
+	private $beerName;
+	/** string used to capture the industry standard style label of the beer
+	 * @var string $beerStyle
+	 **/
+	private $beerName;
 }
