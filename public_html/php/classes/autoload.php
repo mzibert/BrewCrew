975469e03640f7054ea76db581777b2e1282e775
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-05-02 16:37:48 -0600

    Changed namespace in autoload.php
    https://app.asana.com/0/117435671931079/118830311351353

--- public_html/php/classes/autoload.php
+++ public_html/php/classes/autoload.php
@@ -15,7 +15,7 @@ spl_autoload_register(function($class) {
 	 * prefix: the prefix for all the classes (i.e., the namespace)
 	 * baseDir: the base directory for all classes (default = current directory)
 	 **/
-	$prefix = "Edu\\Cnm\\Agraham14\\BrewCrew";
+	$prefix = "Edu\\Cnm\\BrewCrew";
 	$baseDir = __DIR__;
 
 	// does the class use the namespace prefix?
