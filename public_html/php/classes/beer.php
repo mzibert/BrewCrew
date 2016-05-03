451361c41aa171f6e22d1644e8d45eb718f7823c
merri zibert <mzibert@cnm.edu>
2016-05-03 10:44:40 -0600

    finished accessor and mutator for beer availablilty https://app.asana.com/0/117423711360981/118830311351351

--- public_html/php/classes/beer.php
+++ public_html/php/classes/beer.php
@@ -54,7 +54,7 @@ class beer {
 /**
  * accessor method for beer id
  * @return int value of beer id
- */
+ **/
 	public function getbeerId(){
 		return($this->beerId);
 }
@@ -72,4 +72,67 @@ class beer {
 		}
 	//convert and store the beer id
 	$this->beerId = $newbeerId;
+}
+/**accessor method for brewery id
+ * @return int value of brewery id
+ **/
+	public function getbeerBreweryId(){
+		return($this->beerBreweryId);
+}
+/**
+ * mutator method for brewery id
+ *
+ * @param int $newbeerBreweryId new value of brewery id
+ * @throws \RangeException if $newbeerBreweryId is not positive
+ * @throws \TypeError if $newbeerBreweryId is not an integer
+ **/
+	public function setbeerBreweryId($newbeerBreweryId){
+	//verify the brewery id is positive
+		if($newbeerBreweryId <=0){
+			throw (new \RangeException("brewery id is not positive"));
+		}
+		//convert and store the new brewery id
+		$this->breweryId=$newbeerBreweryId;
+}
+/**
+ * accessor method for beer abv
+ * @return float value for beer abv
+ **/
+	public function getbeerAbv(){
+		return($this->beerAbv);
+}
+/**
+ * mutator method for beer abv
+ *
+ * @param float $newbeerAbv new value of beer abv
+ * @throws \RangeException if $newbeerAbv is less than zero or greater than 100%
+ * @throws \TypeError if $newbeerAbv is not a float
+ **/
+	public function setbeerAbv($newbeerAbv){
+	//verify the beer abv is between 0% and 100%
+		if($newbeerAbv < 0 || $newbeerAbv > 100){
+			throw (new \RangeException ("beer abv is out of range"));
+	}
+	//convert and store the new beer abv
+	$this->beerAbv=$newbeerAbv;
+}
+/**
+ * accessor method for beer availability
+ * @return string for beer availability
+ **/
+	public function getbeerAvailability(){
+		return($this->beerAvailability);
+}
+/**
+ *mutator method for beer availablity
+ *
+ *@param string $newbeerAvailability string that tells us if beer is available year round or seasonally
+ *@throws \InvalidArgumentException if $newbeerAvailability is not a string or is insecure
+ *@throws \RangeException if $newbeerAvailability is > 100 characters
+ * @throws \TypeError if $newbeerAvailability is not a string
+ **/
+	public function setbeerAvailability(string $newbeerAvailability){
+	//verify the beer availabilty content is secure
+		$newbeerAvailability = trim($newbeerAvailability);
+		$newbeerAvailability = filter_var($newbeerAvailability, FILTER_SANITIZE_STRING);
 }
