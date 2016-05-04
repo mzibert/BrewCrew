4ba7e1b1a4be6c549477ba752b733b785f542627
merri zibert <mzibert@cnm.edu>
2016-05-04 14:00:35 -0600

    cleaned up camel case https://app.asana.com/0/117423711360981/118830311351351

--- public_html/php/classes/beer.php
+++ public_html/php/classes/beer.php
@@ -47,33 +47,33 @@ class beer {
 
 	/**
 	 * constructor for class beer
-	 * @param int $newbeerId new value of beer id
-	 * @param int $newbeerBreweryId new value of brewery id
-	 * @param float $newbeerAbv new value of beer abv
-	 * @param string $newbeerAvailability tells us if beer is available year round or seasonally
-	 * @param string $newbeerAwards tells us all of the awards that this beer has been awarded
-	 * @param float $newbeerColor new value of beer color between 0 and 1
-	 * @param string $newbeerDescription tells about the details of the beer should not exceed 2000 characters
-	 * @param string $newbeerIbu states how many Ibu's are present in the beer
-	 * @param string $newbeerName displays the name of the beer
-	 * @param string $newbeerStyle is used to assign industry style label
+	 * @param int $newBeerId new value of beer id
+	 * @param int $newBeerBreweryId new value of brewery id
+	 * @param float $newBeerAbv new value of beer abv
+	 * @param string $newBeerAvailability tells us if beer is available year round or seasonally
+	 * @param string $newBeerAwards tells us all of the awards that this beer has been awarded
+	 * @param float $newBeerColor new value of beer color between 0 and 1
+	 * @param string $newBeerDescription tells about the details of the beer should not exceed 2000 characters
+	 * @param string $newBeerIbu states how many Ibu's are present in the beer
+	 * @param string $newBeerName displays the name of the beer
+	 * @param string $newBeerStyle is used to assign industry style label
 	 * @throws \InvalidArgumentException if data types are not valid
 	 * @throws \RangeException if data values are out of range (e.g., strings are too long, negative integers)
 	 * @throws \TypeError if data types violate type hints
 	 * @throws \Exception if some other exception occurs
 	 **/
-	public function __construct(int $newbeerId, int $newbeerBreweryId, float $newbeerAbv, string $newbeerAvailability, string $newbeerAwards, float $newbeerColor, string $newbeerDescription, string $newbeerIbu, string $newbeerName, string $newbeerStyle) {
+	public function __construct(int $newBeerId, int $newBeerBreweryId, float $newBeerAbv, string $newBeerAvailability, string $newBeerAwards, float $newBeerColor, string $newBeerDescription, string $newBeerIbu, string $newBeerName, string $newBeerStyle) {
 		try{
-			$this->setbeerId($newbeerId);
-			$this->setbeerBreweryId($newbeerBreweryId);
-			$this->setbeerAbv($newbeerAbv);
-			$this->setbeerAvailability($newbeerAvailability);
-			$this->setbeerAwards($newbeerAwards);
-			$this->setbeerColor($newbeerColor);
-			$this->setbeerDescription($newbeerDescription);
-			$this->setbeerIbu($newbeerIbu);
-			$this->setbeerName($newbeerName);
-			$this->setbeerStyle($newbeerStyle);
+			$this->setBeerId($newBeerId);
+			$this->setBeerBreweryId($newBeerBreweryId);
+			$this->setBeerAbv($newBeerAbv);
+			$this->setBeerAvailability($newBeerAvailability);
+			$this->setBeerAwards($newBeerAwards);
+			$this->setBeerColor($newBeerColor);
+			$this->setBeerDescription($newBeerDescription);
+			$this->setBeerIbu($newBeerIbu);
+			$this->setBeerName($newBeerName);
+			$this->setBeerStyle($newBeerStyle);
 	}		catch(\InvalidArgumentException $invalidArgument){
 			//rethrow the exception to the caller
 				throw(new \InvalidArgumentException($invalidArgument->getMessage(),0,$invalidArgument));
@@ -95,270 +95,271 @@ class beer {
 	 * accessor method for beer id
 	 * @return int value of beer id
 	 **/
-	public function getbeerId() {
+	public function getBeerId() {
 		return ($this->beerId);
 	}
 
 	/**
 	 *mutator method for beer id
 	 *
-	 * @param int $newbeerId new value of beer id
+	 * @param int $newBeerId new value of beer id
 	 * @throws \RangeException if $newbeerId is not positive
-	 * @throws \TypeError if $newbeerId is not an integer
+	 * @throws \TypeError if $newBeerId is not an integer
 	 **/
-	public function setbeerId($newbeerId) {
+	public function setBeerId($newBeerId) {
 		//verify the profile id is positive
-		if($newbeerId <= 0) {
+		if($newBeerId <= 0) {
 			throw (new \RangeException("beer id is not positive"));
 		}
 		//convert and store the beer id
-		$this->beerId = $newbeerId;
+		$this->beerId = $newBeerId;
 	}
 
 	/**accessor method for brewery id
 	 * @return int value of brewery id
 	 **/
-	public function getbeerBreweryId() {
+	public function getBeerBreweryId() {
 		return ($this->beerBreweryId);
 	}
 
 	/**
 	 * mutator method for brewery id
 	 *
-	 * @param int $newbeerBreweryId new value of brewery id
-	 * @throws \RangeException if $newbeerBreweryId is not positive
-	 * @throws \TypeError if $newbeerBreweryId is not an integer
+	 * @param int $newBeerBreweryId new value of brewery id
+	 * @throws \RangeException if $newBeerBreweryId is not positive
+	 * @throws \TypeError if $newBeerBreweryId is not an integer
 	 **/
-	public function setbeerBreweryId($newbeerBreweryId) {
+	public function setBeerBreweryId($newBeerBreweryId) {
 		//verify the brewery id is positive
-		if($newbeerBreweryId <= 0) {
+		if($newBeerBreweryId <= 0) {
 			throw (new \RangeException("brewery id is not positive"));
 		}
 		//convert and store the new brewery id
-		$this->breweryId = $newbeerBreweryId;
+		$this->breweryId = $newBeerBreweryId;
 	}
 
 	/**
 	 * accessor method for beer abv
 	 * @return float value for beer abv
 	 **/
-	public function getbeerAbv() {
+	public function getBeerAbv() {
 		return ($this->beerAbv);
 	}
 
 	/**
 	 * mutator method for beer abv
 	 *
-	 * @param float $newbeerAbv new value of beer abv
+	 * @param float $newBeerAbv new value of beer abv
 	 * @throws \RangeException if $newbeerAbv is less than zero or greater than 100%
 	 * @throws \TypeError if $newbeerAbv is not a float
 	 **/
-	public function setbeerAbv($newbeerAbv) {
+	public function setBeerAbv($newBeerAbv) {
 		//verify the beer abv is between 0% and 100%
-		if($newbeerAbv < 0 || $newbeerAbv > 100) {
+		if($newBeerAbv < 0 || $newBeerAbv > 100) {
 			throw (new \RangeException ("beer abv is out of range"));
 		}
 		//convert and store the new beer abv
-		$this->beerAbv = $newbeerAbv;
+		$this->beerAbv = $newBeerAbv;
 	}
 
 	/**
 	 * accessor method for beer availability
 	 * @return string for beer availability
 	 **/
-	public function getbeerAvailability() {
+	public function getBeerAvailability() {
 		return ($this->beerAvailability);
 	}
 
 	/**
 	 *mutator method for beer availablity
 	 *
-	 * @param string $newbeerAvailability tells us if beer is available year round or seasonally
+	 * @param string $newBeerAvailability tells us if beer is available year round or seasonally
 	 * @throws \InvalidArgumentException if $newbeerAvailability is not a string or is insecure
 	 * @throws \RangeException if $newbeerAvailability is > 100 characters
 	 * @throws \TypeError if $newbeerAvailability is not a string
 	 **/
-	public function setbeerAvailability($newbeerAvailability) {
+	public function setBeerAvailability($newBeerAvailability) {
 		//verify the beer availabilty content is secure
-		$newbeerAvailability = trim($newbeerAvailability);
-		$newbeerAvailability = filter_var($newbeerAvailability, FILTER_SANITIZE_STRING);
-		if(empty($newbeerAvailability) === true){
+		$newBeerAvailability = trim($newBeerAvailability);
+		$newBeerAvailability = filter_var($newBeerAvailability, FILTER_SANITIZE_STRING);
+		if(empty($newBeerAvailability) === true){
 			throw(new \InvalidArgumentException("Beer availability content is either empty or insecure"));
 		}
 		//verify the beer availability content will fit in the database
-		if(strlen($newbeerAvailability) > 100) {
+		if(strlen($newBeerAvailability) > 100) {
 			throw (new \RangeException("string is greater than 100 characters"));
 		}
 		//convert and store the new beer availability
-		$this->beerAvailability = $newbeerAvailability;
+		$this->beerAvailability = $newBeerAvailability;
 	}
 
 	/**
 	 * accessor method for beer awards
 	 * @return string value of beer awards
 	 **/
-	public function getbeerAwards() {
+	public function getBeerAwards() {
 		return ($this->beerAwards);
 	}
 
 	/**
 	 * mutator method for beer awards
-	 * @param string $newbeerAwards tells us all of the awards that this beer has been awarded
+	 * @param string $newBeerAwards tells us all of the awards that this beer has been awarded
 	 * @throws \InvalidArgumentException if $newbeerAwards is not a string or is insecure
 	 * @throws \RangeException if $newbeerAwards is greater that  1000 characters
 	 * @throws \TypeError if $newbeerAwards is not a string
 	 **/
-	public function setbeerAwards($newbeerAwards) {
+	public function setBeerAwards($newBeerAwards) {
 		//verify the beer awards content is secure
-		$newbeerAwards = trim($newbeerAwards);
-		$newbeerAwards = filter_var($newbeerAwards, FILTER_SANITIZE_STRING);
-		if(empty($newbeerAwards) === true){
+		$newBeerAwards = trim($newBeerAwards);
+		$newBeerAwards = filter_var($newBeerAwards, FILTER_SANITIZE_STRING);
+		if(empty($newBeerAwards) === true){
 			throw (new \InvalidArgumentException("beer awards content is either empty or insecure"));
 		}
-		//verify the beer awards content will fit in the database
-		if(strlen($newbeerAwards) > 1000) {
+		//verify the beer awards content will fit in the
+		// database
+		if(strlen($newBeerAwards) > 1000) {
 			throw (new \RangeException("beer awards description is greater than 1000 characters"));
 		}
 		//convert and store the new beer awards
-		$this->beerAwards = $newbeerAwards;
+		$this->beerAwards = $newBeerAwards;
 	}
 
 	/**
 	 * accessor method for beer color
 	 * @return float value of beer color
 	 **/
-	public function getbeerColor() {
+	public function getBeerColor() {
 		return ($this->beerColor);
 	}
 
 	/**mutator method for beer color
-	 * @param float $newbeerColor new value of beer color between 0 and 1
+	 * @param float $newBeerColor new value of beer color between 0 and 1
 	 * @throws \RangeException if $newbeerColor is less than zero or greater than 1
 	 * @throws \TypeError if $newbeerColor is not a float
 	 **/
-	public function setbeerColor($newbeerColor) {
+	public function setBeerColor($newBeerColor) {
 		//verify the beer abv is between 0 and 1
-		if($newbeerColor < 0 || $newbeerColor > 1) {
+		if($newBeerColor < 0 || $newBeerColor > 1) {
 			throw (new \RangeException ("beer color is out of range"));
 		}
 		//convert and store the new beer color
-		$this->beerColor = $newbeerColor;
+		$this->beerColor = $newBeerColor;
 	}
 
 	/**
 	 * accessor method for beer description
 	 * @return string value of beer description
 	 **/
-	public function getbeerDescription() {
+	public function getBeerDescription() {
 		return ($this->beerDescription);
 	}
 
 	/**
 	 * mutator method for beer description
-	 * @param string $newbeerDescription tells about the details of the beer should not exceed 2000 characters
+	 * @param string $newBeerDescription tells about the details of the beer should not exceed 2000 characters
 	 * @throws \InvalidArgumentException if $newbeerDescription is not a string or is insecure
 	 * @throws \RangeException if the string exceeds 2000 characters
 	 **/
-	public function setbeerDescription($newbeerDescription) {
+	public function setBeerDescription($newBeerDescription) {
 		//verify the beer description content is secure
-		$newbeerDescription = trim($newbeerDescription);
-		$newbeerDescription = filter_var($newbeerDescription, FILTER_SANITIZE_STRING);
-		if(empty($newbeerDescription) === true){
+		$newBeerDescription = trim($newBeerDescription);
+		$newBeerDescription = filter_var($newBeerDescription, FILTER_SANITIZE_STRING);
+		if(empty($newBeerDescription) === true){
 			throw (new \InvalidArgumentException("beer description is either empty or insecure"));
 		}
-		if(strlen($newbeerDescription) > 2000) {
+		if(strlen($newBeerDescription) > 2000) {
 			throw (new \RangeException("beer description is greater than 2000 characters"));
 		}
 		//store the beer description content
-		$this->beerDescription = $newbeerDescription;
+		$this->beerDescription = $newBeerDescription;
 	}
 
 	/**
 	 * accessor method for beer ibu
 	 * @return string value for beer ibu
 	 **/
-	public function getbeerIbu() {
+	public function getBeerIbu() {
 		return ($this->beerIbu);
 	}
 
 	/**
 	 * mutator method for beerIbu
-	 * @param string $newbeerIbu states how many Ibu's are present in the beer
+	 * @param string $newBeerIbu states how many Ibu's are present in the beer
 	 * @throws \InvalidArgumentException if $newbeerIbu is not a string or is insecure
 	 * @throws \RangeException if the string exceeds 50 characters
 	 * @throws \TypeError if $newbeerIbu is not a string
 	 **/
-	public function setbeerIbu($newbeerIbu) {
+	public function setBeerIbu($newBeerIbu) {
 		//verify the beer ibu content is secure
-		$newbeerIbu = trim($newbeerIbu);
-		$newbeerIbu = filter_var($newbeerIbu, FILTER_SANITIZE_STRING);
-		if(empty($newbeerIbu) === true) {
+		$newBeerIbu = trim($newBeerIbu);
+		$newBeerIbu = filter_var($newBeerIbu, FILTER_SANITIZE_STRING);
+		if(empty($newBeerIbu) === true) {
 			throw (new \InvalidArgumentException("beer IBU content is either empty or insecure"));
 		}
 		//verify the tweet content will fit in the database
-		if(strlen($newbeerIbu) > 50) {
+		if(strlen($newBeerIbu) > 50) {
 			throw (new \RangeException("beer IBU contains more than 50 characters"));
 		}
 		//store the beer IBU content
-		$this->beerIbu = $newbeerIbu;
+		$this->beerIbu = $newBeerIbu;
 	}
 
 	/**
 	 * accessor method for beer name
 	 * @return string for for beer name not null
 	 **/
-	public function getbeerName() {
+	public function getBeerName() {
 		return ($this->beerName);
 	}
 
 	/**
 	 * mutator method for beer name
-	 * @param string $newbeerName displays the name of the beer
+	 * @param string $newBeerName displays the name of the beer
 	 * @throws \InvalidArgumentException if $newbeerName is not a string or is insecure
 	 * @throws \RangeException if string exceeds 64 characters
 	 * @throws \TypeError if $newbeerName is not a string
 	 **/
-	public function setbeerName($newbeerName) {
+	public function setBeerName($newBeerName) {
 		//verify the beer name content is secure
-		$newbeerName = trim($newbeerName);
-		$newbeerName = filter_var($newbeerName, FILTER_SANITIZE_STRING);
-		if (empty($newbeerName)=== true){
+		$newBeerName = trim($newBeerName);
+		$newBeerName = filter_var($newBeerName, FILTER_SANITIZE_STRING);
+		if (empty($newBeerName)=== true){
 			throw(new \InvalidArgumentException("beer name is either empty or insecure"));
 		}
 		//verify the beer name content will fit in the database
-		if(strlen($newbeerName) > 64) {
+		if(strlen($newBeerName) > 64) {
 			throw (new \RangeException("beer name contains more than 64 characters"));
 		}
 		//store the beer name
-		$this->beerName = $newbeerName;
+		$this->beerName = $newBeerName;
 	}
 
 	/**
 	 * accessor method for beer style
 	 * @return string to describe industry style label
 	 **/
-	public function getbeerStyle() {
+	public function getBeerStyle() {
 		return ($this->beerStyle);
 	}
 
 	/**
 	 * mutator method for beer stlye
-	 * @param string $newbeerStyle is used to assign industry style label
+	 * @param string $newBeerStyle is used to assign industry style label
 	 * @throws \InvalidArgumentException if $newbeerStyle content is not a string or is insecure
 	 * @throws \RangeException if string exceeds 32 characters
 	 **/
-	public function setbeerStyle($newbeerStyle) {
+	public function setBeerStyle($newBeerStyle) {
 		//verify the beer style content is secure
-		$newbeerStyle = trim($newbeerStyle);
-		$newbeerStyle = filter_var($newbeerStyle, FILTER_SANITIZE_STRING);
-		if(empty($newbeerStyle) === true){
+		$newBeerStyle = trim($newBeerStyle);
+		$newBeerStyle = filter_var($newBeerStyle, FILTER_SANITIZE_STRING);
+		if(empty($newBeerStyle) === true){
 			throw (new \InvalidArgumentException("beer style content is either empty or insecure"));
 		}
 		//verify the content of beer style can fit in the database
-		if(strlen($newbeerStyle) > 32) {
+		if(strlen($newBeerStyle) > 32) {
 			throw (new \RangeException("beer style is greater than 32 characters"));
 		}
 		//store the beer style content
-		$this->beerStyle = $newbeerStyle;
+		$this->beerStyle = $newBeerStyle;
 	}
 }
