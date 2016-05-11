f622ed981c481d3cf9a3eda832799e58eff6dd7b
Arlene Graham <agraham14@cnm.edu>
2016-05-11 16:48:56 -0600

    updated UserTest.php how in the world did I ever think I was finished the last time?????
    https://app.asana.com/0/117435671931146/118830311351347

--- public_html/test/UserTest.php
+++ public_html/test/UserTest.php
@@ -18,126 +18,137 @@ require_once (dirname(__DIR__) . "../php/classes/autoload.php");
  **/
 class UserTest extends BrewCrewTest {
 	/**
-	 * user
-	 * @var string $VALID_USER
+	 * Valid userId
+	 * @var int
+	 */
+	protected $VALID_USERID = "10";
+
+
+	protected $VALID_ACCESSLEVEL = "PHPUnit test passing";
+	/**
+	 * @var int $newUserActivationToken int with user token
+	 */
+	protected $VALID_ACTIVATIONTOKEN = "PHPUnit test passing";
+	/**
+	 * @var \DateTime $newUserDateOfBirth date User was sent or null if set to current date and time
+	 */
+	protected $VALID_DATEOFBIRTH = "PHPUnit test passing";
+	/**
+	 * @var string $newUserEmail string containing user email
 	 */
 	protected $VALID_EMAIL = "PHPUnit test passing";
-
+	/**
+	 * @var string $newUserEmail string containing user email
+	 */
 	protected $VALID_EMAIL2 = "PHPUnit test passing again";
+
+	/**
+	 * valid firstName of userId
+	 * @var string $firstName
+	 **/
+	protected $VALID_FIRSTNAME = "PHPUnit test passing";
+	/**
+	 * valid lastName of userId
+	 * @var string $lastName
+	 **/
+	protected $VALID_LASTNAME = "PHPUnit test passing";
 	/**
 	 * user
-	 * @var string $VALID_USER2
+	 * @var string userUsername
 	 **/
 	protected $VALID_USERUSERNAME = "PHPUnit test still passing";
 
 	protected $brewery = null;
+
+	private $salt;
+	private $hash;
+	private $password = "password";
 
 	//create dependent objects before running each test
 	public final function setUp () {
 		//run the default setUp() method first
 		parent::setUp();
 		//create and insert a Brewery to own the test user
-		$this->brewery = new Brewery(null, "@phpunit", "@testphpunit.de", "12125551212");
+		$this->brewery = new Brewery(null, "brewery description", "2016", "7am - 11pm", "location", "Marble Brewery", "15055551212", "Marble@Marble.com");
 		$this->brewery->insert($this->getPDO());
 		$this->VALID_EMAIL = new \User();
 
 	}
 
-	//test inserting a valid email and verify that it matches the mySQL data
-	public function testInsertValidUser ($pdoUserUsername, $pdoBrewery) {
+	//test inserting a valid USER and verify that it matches the mySQL data
+	public function testInsertValidUser () {
 		//count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("Email");
+		$numRows = $this->getConnection()->getRowCount("User");
+
+		//create salt and hash for user
+		$this->salt = bin2hex(16);
+		$this->hash = hash_pbkdf2("sha512", $this->password, $this->salt, 4096);
+
 		//create a new user and insert it into mySQL
-		$User = new User(null, $this->User->getUserId(), $this->VALID_EMAIL, $this->VALID_USERUSERNAME);
+		$User = new User(null, $this->VALID_ACCESSLEVEL,$this->VALID_ACTIVATIONTOKEN, $this->VALID_DATEOFBIRTH, $this->VALID_EMAIL,$this->VALID_FIRSTNAME,$this->hash,$this->VALID_LASTNAME, $this->salt,$this->VALID_USERUSERNAME);
 		$User->insert($this->getPDO());
+
 		//grab the data from mySQL and enforce the fields to match our expectations
 		$pdoUser = User::getUserbyUserID($this->getPDO(), $User->getUserId());
+
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("user"));
-		$this->assertEquals($pdoUser->getUserEmail(), $this->User->getUserEmail());
-		$this->assertEquals($pdoUserUsername->getUserUsername(), $this->userUsername->getUserUsername());
-		$this->assertEquals($pdoBrewery->getBrewery(), $this->Brewery->getBrewery());
+		$this->assertSame($pdoUser->getUserAccessLevel(), $this->VALID_ACCESSLEVEL);
+		$this->assertSame($pdoUser->getUserActivationToken(), $this->VALID_ACTIVATIONTOKEN);
+		$this->assertSame($pdoUser->getUserDateOfBirth(), $this->VALID_DATEOFBIRTH);
+		$this->assertSame($pdoUser->getUserEmail(), $this->VALID_EMAIL);
+		$this->assertSame($pdoUser->getUserFirstName(), $this->VALID_FIRSTNAME);
+		$this->assertSame($pdoUser->getUserHash(), $this->hash);
+		$this->assertSame($pdoUser->getUserLastName(), $this->VALID_LASTNAME);
+		$this->assertSame($pdoUser->getUserSalt(), $this->salt);
+		$this->assertSame($pdoUser->getUserUsername(), $this->VALID_USERUSERNAME);
 
 	}
-	//test inserting something that already exists
-	//expecting PDOException
-	public function testInsertInvalidUser($getUserId) {
-		$User = new User(UserTest::INVALID_KEY, $this->User->$getUserId(), $this->VALID_EMAIL, $this->VALID_USERNAME);
+	/**
+	 * test inserting a User that already exists
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testInsertInvalidUser() {
+		// create a profile with a non null profileId and watch it fail
+
+		$User = new User(null, $this->VALID_ACCESSLEVEL,$this->VALID_ACTIVATIONTOKEN, $this->VALID_DATEOFBIRTH, $this->VALID_EMAIL,$this->VALID_FIRSTNAME,$this->hash,$this->VALID_LASTNAME, $this->salt,$this->VALID_USERUSERNAME);
 		$User->insert($this->getPDO());
-	}
-	//test creating a user, editing it, and then updating it
-	public function testUpdateValidUser($User, $pdoUser) {
-		//count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("user");
-		//create a new user and update it into mySQL
-		$User->setUserbyUserId($this->getPDO(), $User->getUserId());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("user"));
-		$this->assertEquals($pdoUser->getUserId(), $this->user->getUserId());
-		$this->assertEquals($pdoUser->getEmail(), $this->VALID_EMAIL);
-		$this->assertEquals($pdoUser->getBREWERY(), $this->VALID_BREWERY);
-	}
-	//test updating a user that already exists
-//@expectedException PDOException
-	public function testUpdateInvalidUser($User) {
-		//create a user with a non null user id to watch it fail
-		$User = new User(null, $this->User->getUserId(), $this->VALID_EMAIL, $this->VALID_BREWERY);
-		$User->update($this->getPDO());
-	}
-	//test creating a User then deleting it
-	public function testDeleteValidUser() {
-		//count rows
-		$numRows = $this->getConnection()->getRowCount("User");
-		//create a new User and insert into mySQL
-		$User = new User(null, $this->User->getUserId(), $this->VALID_EMAIL, $this->VALID_BREWERY);
 		$User->insert($this->getPDO());
-		//delete this User from mySQL
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("User"));
-		$User->delere($this->getPDO());
-		//grab the data from mySQL and enforce the User does not exist
-		$pdoUser = User::GetUserbyUserId($this->getPDO(), $User->getUserId());
-		$this->assertNul($pdoUser);
-		$User->assertEquals($numRows, $this->User->getConnection()->getRowCount("User"));
-	}
-	//test grabbing a User that does not exist
-	public function testGetInvalidUserbyUserId() {
-		//grab a profile id that exceeds the maximum allowable profile id
-		$User = User::getUserbyUserId($this->getPDO(), UserTest::INVALID_KEY);
-		$this->assertNull($User);
-	}
-//test grabbing a User by Email
-	public function testGetInvalidUserByEmail($User) {
-		//count number of rows
-		$numRows = $this->getConnection()->getRowCount("User");
-		// create a new User and insert to into mySQL
-		$tweet = new User(null, $this->UserEmail->getUserEmailId(), $this->VALID_EMAIL, $this->VALID_BREWERY);
-		$tweet->insert($this->getPDO());
-		// grab the data from mySQL and enforce the fields match our expectations
-		$results = User::getUserByUserId($this->getPDO(), $User->getUserEmail());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("User"));
-		$this->assertCount(1, $results);
-		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\BrewCrew\\Test\\UserTest", $results);
-		// grab the result from the array and validate it
-		$pdoUser = $results[0];
-		$this->assertEquals($pdoUser->getUserId(), $this->User->getUserId());
-		$this->assertEquals($pdoUser->getUserEmail(), $this->VALID_EMAIL);
-		$this->assertEquals($pdoUser->getBrewery(), $this->VALID_BREWERY);
 	}
 
-	//test grabbing all users
-	public function testGetAllValidUsers() {
+	/**
+	 * test inserting a User, editing it, and then updating it
+	 **/
+	public function testUpdateValidUser() {
 		// count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("User");
+		$numRows = $this->getConnection()->getRowCount("user");
 		// create a new User and insert to into mySQL
-		$User = new User(null, $this->User->getUserId(), $this->VALID_EMAIL, $this->VALID_BREWERY);
+		$User = new User(null, $this->VALID_ACCESSLEVEL, $this->VALID_ACTIVATIONTOKEN, $this->VALID_DATEOFBIRTH, $this->VALID_EMAIL, $this->VALID_FIRSTNAME, $this->hash, $this->VALID_LASTNAME, $this->salt, $this->VALID_USERUSERNAME);
 		$User->insert($this->getPDO());
+
+		$User->insert($this->getPDO());
+		// edit the user and update it in mySQL
+		$User->setUserEmail($this->VALID_EMAIL2);
+		$User->update($this->getPDO());
+
+
 		// grab the data from mySQL and enforce the fields match our expectations
-		$results = User::getAllUsers($this->getPDO());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("Users"));
-		$this->assertCount(1, $results);
-		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\BrewCrew\\Test\\UserTest", $results);
-		// grab the result from the array and validate it
-		$pdoUser = $results[0];
-		$this->assertEquals($pdoUser->getUserId(), $this->User->getUserId());
-		$this->assertEquals($pdoUser->getUserEmail(), $this->VALID_EMAIL);
-		$this->assertEquals($pdoUser->getBrewery(), $this->VALID_LBREWERY);
+
+		$pdoUser = User::getUserByUserId($this->getPDO(), $User->getUserId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("user"));
+
+		$this->assertSame($pdoUser->getUserAccessLevel(), $this->VALID_ACCESSLEVEL);
+		$this->assertSame($pdoUser->getUserActivationToken(), $this->VALID_ACTIVATIONTOKEN);
+		$this->assertSame($pdoUser->getUserDateOfBirth(), $this->VALID_DATEOFBIRTH);
+		$this->assertSame($pdoUser->getUserEmail(), $this->VALID_EMAIL2);
+		$this->assertSame($pdoUser->getUserFirstName(), $this->VALID_FIRSTNAME);
+		$this->assertSame($pdoUser->getUserHash(), $this->hash);
+		$this->assertSame($pdoUser->getUserLastName(), $this->VALID_LASTNAME);
+		$this->assertSame($pdoUser->getUserSalt(), $this->salt);
+		$this->assertSame($pdoUser->getUserUsername(), $this->VALID_USERUSERNAME);
+
+
 	}
+
+
 }
