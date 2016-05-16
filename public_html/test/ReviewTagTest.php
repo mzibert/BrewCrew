c973daa94c1261339adaa5af6ef6e3b048c1f566
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-05-16 10:21:05 -0600

    fixes to order of classes in setup, foreign keys
    https://app.asana.com/0/117435671931079/118830311351359

--- public_html/test/ReviewTagTest.php
+++ public_html/test/ReviewTagTest.php
@@ -28,16 +28,18 @@ class ReviewTagTest extends BrewCrewTest {
 	protected $brewery = null;
 
 	/**
+	 * User that wrote the review; this is for foreign key relations
+	 * @var User user
+	 */
+	protected $user = null;
+
+	/**
 	 * Beer that is being reviewed; this is for foreign key relations
 	 * @var Beer beer
 	 */
 	protected $beer = null;
 
-	/**
-	 * User that wrote the review; this is for foreign key relations
-	 * @var User user
-	 */
-	protected $user = null;
+
 	/**
 	 * Review that contains the associated tags
 	 * @var Review review
@@ -60,18 +62,19 @@ class ReviewTagTest extends BrewCrewTest {
 		$this->brewery = new Brewery(null, "description of Test Brewery", "2005", "24/7 8 days a week", "The middle of nowhere aka Rolla, MO", "Test Brewery", "1238675309", "www.awesometestbrewery.com");
 		$this->brewery->insert($this->getPDO());
 
-		//create and insert a beer that is being reviewed
-		$this->beer = new Beer(null, $this->brewery->getBreweryId, 10.5, "available whenever we like it to be", "lots of awards we don't care to name", .5, "something something about the taste.", "50", "Teh Awesome Beer", "Lager");
-		$this->beer->insert($this->getPDO());
-
 		//salt and hash generation
 		$password = "hunter2";
 		$salt = bin2hex(random_bytes(16));
 		$hash = hash_pbkdf2("sha512", $password, $salt, 262144);
 
 		//create and insert a user that owns the review
-		$this->user = new User(null, null, 0, "token", '1980-01-01', "anon@test.com", "John", $hash, "Smith", $salt, "beerfan4lyfe");
+		$this->user = new User(null, $this->brewery->getBreweryId(), 0, "token", '1980-01-01', "anon@test.com", "John", $hash, "Smith", $salt, "beerfan4lyfe");
 		$this->user->insert($this->getPDO());
+
+		//create and insert a beer that is being reviewed
+		$this->beer = new Beer(null, $this->brewery->getBreweryId, 10.5, "available whenever we like it to be", "lots of awards we don't care to name", .5, "something something about the taste.", "50", "Teh Awesome Beer", "Lager");
+		$this->beer->insert($this->getPDO());
+
 
 		//create a review for the tags to be associated with
 		$this->review = new Review(null, $this->beer->getBeerId, $this->user->getUserId, new \DateTime(), 5, "this beer was really good. I liked it a lot.");
@@ -91,14 +94,14 @@ class ReviewTagTest extends BrewCrewTest {
 		$numRows = $this->getConnection()->getRowCount("reviewTag");
 
 		//create a new reviewTag and insert it into mySQL
-		$reviewTag = new ReviewTag($this->reviewTag->getReviewTagReviewId(), $this->reviewtag->getReviewTagTagId());
+		$reviewTag = new ReviewTag($this->review->getReviewId(), $this->tag->getTagId());
 		$reviewTag->insert($this->getPDO());
 
 		//grab the data from mySQL and enforce that the fields match our expectations
 		$pdoReviewTag = ReviewTag::getReviewTagByReviewId($this->getPDO(), $reviewTag->getReviewTagReviewId());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("reviewTag"));
-		$this->assertEquals($pdoReviewTag->getReviewTagReviewId(), $this->reviewTag->getReviewTagReviewId());
-		$this->assertEquals($pdoReviewTag->getReviewTagTagId(), $this->reviewTag->getReviewTagTagId());
+		$this->assertEquals($pdoReviewTag->getReviewTagReviewId(), $this->review->getReviewId());
+		$this->assertEquals($pdoReviewTag->getReviewTagTagId(), $this->tag->getTagId());
 	}
 
 	/**
@@ -107,7 +110,7 @@ class ReviewTagTest extends BrewCrewTest {
 	 */
 	public function testInsertInvalidReviewTag() {
 		//create a reviewTag with a non-null id and watch it fail
-		$reviewTag = new ReviewTag(BrewCrewTest::INVALID_KEY, $this->reviewTag->getReviewTagTagId());
+		$reviewTag = new ReviewTag(BrewCrewTest::INVALID_KEY, $this->tag->getTagId());
 		$reviewTag->insert($this->getPDO());
 	}
 
@@ -117,7 +120,7 @@ class ReviewTagTest extends BrewCrewTest {
 	 */
 	public function testUpdateInvalidReviewTag() {
 		//create a reviewTag with a non-null id and watch it fail
-		$reviewTag = new ReviewTag($this->reviewTag->getReviewTagReviewId(), $this->reviewTag->getReviewTagTagId());
+		$reviewTag = new ReviewTag($this->review->getReviewId(), $this->tag->getTagId());
 		$reviewTag->update($this->getPDO());
 	}
 
@@ -130,7 +133,7 @@ class ReviewTagTest extends BrewCrewTest {
 		$numRows = $this->getConnection()->getRowCount("reviewTag");
 
 		//create a new reviewTag and insert it into mySQL
-		$reviewTag = new ReviewTag($this->reviewTag->getReviewTagReviewId(), $this->reviewTag->getReviewTagTagId());
+		$reviewTag = new ReviewTag($this->review->getReviewId(), $this->tag->getTagId());
 		$reviewTag->insert($this->getPDO());
 
 		//delete the reviewTag from mySQL
@@ -150,7 +153,7 @@ class ReviewTagTest extends BrewCrewTest {
 	 */
 	public function testDeleteInvalidReviewTag() {
 		//create a reviewTag and try to delete it without actually inserting it
-		$reviewTag = new ReviewTag($this->reviewTag->getReviewTagReviewId(), $this->reviewTag->getReviewTagTagId());
+		$reviewTag = new ReviewTag($this->review->getReviewId(), $this->tag->getTagId());
 		$reviewTag->delete($this->getPDO());
 	}
 
@@ -163,7 +166,7 @@ class ReviewTagTest extends BrewCrewTest {
 		$numRows = $this->getConnection()->getRowCount("reviewTag");
 
 		//create a new reviewTag and insert it into mySQL
-		$reviewTag = new ReviewTag($this->reviewTag->getReviewTagReviewId(), $this->reviewTag->getReviewTagTagId());
+		$reviewTag = new ReviewTag($this->review->getReviewId(), $this->tag->getTagId());
 		$reviewTag->insert($this->getPDO());
 
 		//grab the data from mySQL and enforce that the fields match our expectations
@@ -174,8 +177,8 @@ class ReviewTagTest extends BrewCrewTest {
 
 		//grab the results from the array and validate them
 		$pdoReviewTag = $results[0];
-		$this->assertEquals($pdoReviewTag->getReviewTagReviewId(), $this->reviewTag->getReviewTagReviewId());
-		$this->assertEquals($pdoReviewTag->getReviewTagTagId(), $this->reviewTag->getReviewTagTagId());
+		$this->assertEquals($pdoReviewTag->getReviewTagReviewId(), $this->review->getReviewId());
+		$this->assertEquals($pdoReviewTag->getReviewTagTagId(), $this->tag->getTagId());
 
 	}
 
@@ -198,7 +201,7 @@ class ReviewTagTest extends BrewCrewTest {
 		$numRows = $this->getConnection()->getRowCount("reviewTag");
 
 		//create a new reviewTag and insert it into mySQL
-		$reviewTag = new ReviewTag($this->reviewTag->getReviewTagReviewId(), $this->reviewTag->getReviewTagTagId());
+		$reviewTag = new ReviewTag($this->review->getReviewTag(), $this->tag->getTagId());
 		$reviewTag->insert($this->getPDO());
 
 		//grab the data from mySQL and enforce that the fields match our expectations
@@ -209,8 +212,8 @@ class ReviewTagTest extends BrewCrewTest {
 
 		//grab the results from the array and validate them
 		$pdoReviewTag = $results[0];
-		$this->assertEquals($pdoReviewTag->getReviewTagReviewId(), $this->reviewTag->getReviewTagReviewId());
-		$this->assertEquals($pdoReviewTag->getReviewTagTagId(), $this->reviewTag->getReviewTagTagId());
+		$this->assertEquals($pdoReviewTag->getReviewTagReviewId(), $this->review->getReviewId());
+		$this->assertEquals($pdoReviewTag->getReviewTagTagId(), $this->tag->getTagId());
 	}
 
 	/**
@@ -232,7 +235,7 @@ class ReviewTagTest extends BrewCrewTest {
 		$numRows = $this->getConnection()->getRowCount("reviewTag");
 
 		//create a new reviewTag and insert it into mySQL
-		$reviewTag = new ReviewTag($this->reviewTag->getReviewTagReviewId(), $this->reviewTag->getReviewTagTagId());
+		$reviewTag = new ReviewTag($this->review->getReviewId(), $this->tag->getTagId());
 		$reviewTag->insert($this->getPDO());
 
 		//grab the data from mySQL and enforce that the fields match our expectations
@@ -243,8 +246,8 @@ class ReviewTagTest extends BrewCrewTest {
 
 		//grab the results from the array and validate them
 		$pdoReviewTag = $results[0];
-		$this->assertEquals($pdoReviewTag->getReviewTagReviewId(), $this->reviewTag->getReviewTagReviewId());
-		$this->assertEquals($pdoReviewTag->getReviewTagTagId(), $this->reviewTag->getReviewTagTagId());
+		$this->assertEquals($pdoReviewTag->getReviewTagReviewId(), $this->review->getReviewId());
+		$this->assertEquals($pdoReviewTag->getReviewTagTagId(), $this->tag->getTagId());
 	}
 
 	/**
