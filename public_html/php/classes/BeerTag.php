39fa6e00553ac5bb10bf7f3d2eb05400f47376e4
merri zibert <mzibert@cnm.edu>
2016-05-17 22:13:18 -0600

    working on PDO of beertag class https://app.asana.com/0/117423711360981/118830311351357

--- public_html/php/classes/BeerTag.php
+++ public_html/php/classes/BeerTag.php
@@ -3,6 +3,12 @@ namespace Edu\Cnm\BrewCrew;
 
 require_once ("autoload.php");
 
+/**
+ * Class BeerTag
+ * This class contains everything for a user to be able to link beer flavor tags with the beers that they drink
+ * @author Merri Zibert <mzibert@cnm.edu>
+ **/
+
 class BeerTag {
 	/**
 	 * this is the Id for this beer that this beer tag refers to, this is the foreign key
@@ -14,16 +20,22 @@ class BeerTag {
 	 * @var int $beerTagTagId
 	 **/
 	private $beerTagTagId;
-
+	
 	/**
-	 * Constructor for class beerTag
+	 * Constructor for class BeerTag
 	 * @param int $newBeerTagBeerId new value of beer tag beer Id
 	 * @param int $newBeerTagTagId new value of the tag id assigned to this beer
+	 * @throws \InvalidArgumentException if the data types are not valid
+	 * @throws \TypeError if the data entered is the incorrect type
+	 * @throws \Exception if any other type of errors occur
 	 **/
 	public function __construct(int $newBeerTagBeerId, int $newBeerTagTagId) {
 		try {
 			$this->setBeerTagBeerId($newBeerTagBeerId);
 			$this->setBeerTagTagId($newBeerTagTagId);
+		} catch(\InvalidArgumentException $InvalidArgument){
+			//rethrow the exception to the caller
+			throw (\InvalidArgumentException($InvalidArgument->getMessage(), 0, $InvalidArgument));
 		} catch(\RangeException $range) {
 			//rethrow the exception to the caller
 			throw (new \RangeException($range->getMessage(), 0, $range));
@@ -39,15 +51,15 @@ class BeerTag {
 	 */
 	/**
 	 * accessor method for beerTagBeerId
-	 * @return int value of beer Tag Beer Id
+	 * @return int value of beerTag BeerId, foreign key
 	 **/
 	public function getBeerTagBeerId() {
 		return ($this->beerTagBeerId);
 	}
 
 	/**
-	 * mutator method for beer tag beer Id
-	 * @param int $newBeerTagBeerId new value of beer tag beer Id
+	 * mutator method for beerTagBeerId
+	 * @param int $newBeerTagBeerId new value of beerTag beerId
 	 * @throws \RangeException if $newBeerTagBeerId is not positive
 	 * @throws \TypeError if $newBeerTagBeerId is not an integer
 	 **/
@@ -62,14 +74,14 @@ class BeerTag {
 
 	/**
 	 * accessor method for beerTagTagId
-	 * @return int value of beer tag tag Id
+	 * @return int value of beerTag tagId, foreign key
 	 **/
 	public function getBeerTagTagId() {
 		return ($this->beerTagTagId);
 	}
 
 	/**
-	 * mutator method for beer tag tag id
+	 * mutator method for beerTag tagId
 	 * @param int $newBeerTagTagId new value of the tag id assigned to this beer
 	 * @throws \RangeException if $newBeerTagTagId is not positive
 	 * @throws \TypeError if $newBeerTagTagId is not an integer
@@ -88,8 +100,62 @@ class BeerTag {
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError if $pdo is not a PDO connection object
 	 **/
-	public function insert(\PDO $pdo){
-		//enforce the beerTag is null (i.e., dont insert a beer tag that already exists)
-		if($this->)
+	public function insert(\PDO $pdo) {
+		//check that the beer tag exists before inserting into SQL
+		if($this->beerTagBeerId === null || $this->beerTagTagId === null){
+			throw (new \PDOException("beer or tag not valid"));
+		}
+		//create query template
+		$query = "INSERT INTO beerTag(beerTagBeerId, beerTagTagId) VALUES (:beerTagBeerId, :beerTagTagId)";
+		$statement = $pdo->prepare($query);
+
+		//bind the member variables to the place holders in the template
+		$parameters = ["beerTagBeerId" => $this->beerTagBeerId, "beerTagTagId" => $this->beerTagTagId];
+		$statement->execute($parameters);
+		//update the null beerId with what my SQL just gave us
+		
+		
 	}
 }
+
+/**
+ * deletes this beer tag from mySQL
+ * @param \PDO $pdo PDO connection object
+ * @throws \PDOException when mySQL related errors occure
+ * @throws \TypeError if $pdo is not a PDO connection object
+ **/
+
+//enforce the beerTagID is not null (i.e., don't delete a beer tag that has not been inserted)
+
+//create a query template
+
+//bind the member variables to the place holder in the template
+
+
+/**
+ * updates this Beer Tag in mySQL
+ *
+ * @param \PDO $pdo PDO connection object
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError if $pdo is not a PDO connection object
+ **/
+
+//create query template
+
+//bind the member variables to the place holders in the template
+
+/**
+ * gets the Beer Tag by beerTag ID
+ *
+ * @param \PDO $pdo PDO connection object
+ * @param int $beerId the beerId to search for
+ * @return \Beer|null either the beer or null if not found
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError when variables are not the correct data type
+ **/
+
+//create query template
+
+//bind the beer id to the place holder in the template
+
+//grab the beer from mySQL
