8d3d04f90e4d5d316326fde978512f8fd8b369be
merri zibert <mzibert@cnm.edu>
2016-05-23 16:59:05 -0600

    started the api

---
+++ public_html/php/apis/beer/index.php
@@ -0,0 +1,42 @@
+<?php>
+/**
+ * GET all beers
+ * GET a specific beer by primary key
+ * GET beers by brewery Id
+ * GET beer by IBU
+ * GET beer by beer color
+ * GET beer by beer name
+ * GET beer by style
+ * POST create a brand new beer
+ * PUT update beer
+ * DELETE a beer
+ **/
+require_once "autoloader.php";
+require_once "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\BrewCrew;
+
+/**
+ * api for the beer class
+ *
+ * @author Merri J. Zibert <mjzibert2@gmail.com>
+ */
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE){
+	session_start();
+}
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try{
+	//grab the mySQL connection
+	$pdo = ConnectToEncryptedMySQL("/etc/apache2/capstone-mysql/brewcrew.ini");
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+}
+
