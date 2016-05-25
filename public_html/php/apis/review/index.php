3d132f9dbd608a9b0c1ecbe14703e161f7051517
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-05-25 15:46:57 -0600

    started work on api for review
    https://app.asana.com/0/117435671931079/118830311351353

---
+++ public_html/php/apis/review/index.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once "autoloader.php";
+require_once "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\BrewCrew;
+
+/**
+ * api for the review class
+ *
+ * @author Alicia Broadhurst <abroadhurst@cnm.edu>
+ */
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/brewcrew.ini");
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD",$_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+
+	//make sure the id is valid for methods that require it
+	if(($method === "DELETE") && (empty($id) === true || $id < 0)) {
+		throw(new \InvalidArgumentException("id cannot be empty or negative", 405));
+	}
+
+	//handle GET request - if id is present, then the review is returned, otherwise all reviews are returned
+	if($method == "GET") {
+		//set XSRF cookie
+		setXsrfCookie();
+
+
+	}
+
+
+
+
+
+
+
+}
