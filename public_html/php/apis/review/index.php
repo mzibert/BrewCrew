d80b608a7bf5d1e48c0a64794891d607073baa7c
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-05-25 20:34:03 -0600

    making progress on API, about halfway
    https://app.asana.com/0/117435667927162/118830311351353

--- public_html/php/apis/review/index.php
+++ public_html/php/apis/review/index.php
@@ -31,6 +31,11 @@ try {
 
 	//sanitize input
 	$reviewId = filter_input(INPUT_GET, "reviewId", FILTER_VALIDATE_INT);
+	$breweryId = filter_input(INPUT_GET, "breweryId", FILTER_VALIDATE_INT);
+	$userId = filter_input(INPUT_GET, "userId", FILTER_VALIDATE_INT);
+	$beerId = filter_input(INPUT_GET, "beerId", FILTER_VALIDATE_INT);
+	$reviewPintRating = filter_input(INPUT_GET, "reviewPintRating", FILTER_VALIDATE_INT);
+	
 
 	//make sure the id is valid for methods that require it
 	if(($method === "DELETE") && (empty($reviewId) === true || $reviewId < 0)) {
@@ -41,9 +46,53 @@ try {
 	if($method == "GET") {
 		//set XSRF cookie
 		setXsrfCookie();
+		
+		//get methods:get a specific review by reviewId, get reviews by beerId, by userId, by breweryId, and by pintRating
+		if(empty($reviewId) === false) {
+			$review = BrewCrew\Review::getReviewByReviewId($pdo, $reviewId);
+			if($review !== null) {
+				$reply->data = $review;
+			}
+		} else if(empty($beerId) === false) {
+			$review = BrewCrew\Review::getReviewByBeerId($pdo, $review);
+			if($review !== null) {
+				$reply->data = $review;
+			}
+		} else if(empty($userId) === false) {
+			$review = BrewCrew\Review::getReviewByUserId($pdo, $review);
+			if($review !== null) {
+				$reply->data = $review;
+			}
+		}else if(empty($breweryId) === false) {
+			$review = BrewCrew\Review::getReviewByBreweryId($pdo, $breweryId);
+			if($review !== null) {
+				$reply->data = $review;
+			}
+		}else if(empty($reviewPintRating) === false) {
+			$review = BrewCrew\Review:: getReviewByReviewPintRating($pdo, $breweryId);
+			if($review !== null) {
+				$reply->data = $review;
+			}
+		}
+	} else if($method === "POST") {
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
 
-
-
+		//make sure that content is available
+		if(empty($requestObject->reviewPintRating) === true) {
+			throw(new \InvalidArgumentException ("No pint rating for the review.", 405));
+		}
+	}
+	//perform the actual POST
+	if($method === "POST") {
+		//make sure that the userId if available
+		if(empty($requestObject->userId) === true) {
+			throw(new \InvalidArgumentException ("No associated user ID.", 405));
+		}
+		//create new review and insert it into the database
+		$review = new BrewCrew\Review
+	}
 
 
 
