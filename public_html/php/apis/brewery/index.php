482f9ccdb85324d1b2d4603649840e56c4a18445
McGaughey <therealmcgaughey@gmail.com>
2016-05-26 08:31:00 -0600

    Fixing API for brewery class

--- public_html/php/apis/brewery/index.php
+++ public_html/php/apis/brewery/index.php
@@ -1,8 +1,10 @@
 <?php
 
 require_once "autoloader.php";
+require_once
 require_once "/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+// Do I have to be more specific in my file paths?
 
 use Edu\Cnm\BrewCrew;
 
@@ -13,37 +15,55 @@ use Edu\Cnm\BrewCrew;
  * @author Kate McGaughey <therealmcgaughey@gmail.com>
  **/
 
+// Prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+// This $reply is an empty bucket we store the results of our calls in
+
 // Verify the session, start if not active
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
 
-// Prepare an empty reply
-$reply = new stdClass();
-$reply->status = 200;
-$reply->data = null;
-
 try {
 	// Grab the mySQL connection
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/brewcrew.ini");
+	// Is that the correct file path?
 
 	// Determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	// Stores the result in $method
 
 	// Sanitize input
-	$breweryId = filter_input(INPUT_GET, "breweryId", FILTER_VALIDATE_INT);
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	// NOT breweryId here or ever in the API. This use of id is where the GET, DELETE, and PUT methods store their Primary Keys. If no key is present, $id will remain empty.
 
 	// Make sure the id is valid for methods that require it
-	if(($method === "DELETE" || $method === "PUT") && (empty($breweryId) === true || $breweryId < 0)) {
-		throw(new InvalidArgumentException("Brewery id cannot be empty or negative", 405));
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+		// This is checking to make sure we have the Primary Keys for the DELETE and PUT requests.
 	}
-	// Handle GET request - if breweryId is present, that brewery is returned, otherwise all breweries are returned
+	// Sanitize and trim the rest of the inputs
+	$breweryId = filter_input(INPUT_GET, "breweryId", FILTER_VALIDATE_INT);
+	$breweryDescription = filter_input(INPUT_GET, "breweryDescription", FILTER_VALIDATE_INT);
+	$breweryEstDate = filter_input(INPUT_GET, "breweryEstDate", FILTER_VALIDATE_INT);
+	$breweryHours = filter_input(INPUT_GET, "breweryHours", FILTER_VALIDATE_INT);
+	$breweryLocation = filter_input(INPUT_GET, "breweryLocation", FILTER_VALIDATE_INT);
+	$breweryName = filter_input(INPUT_GET, "breweryName", FILTER_VALIDATE_INT);
+	$breweryPhone = filter_input(INPUT_GET, "breweryPhone", FILTER_VALIDATE_INT);
+	$breweryUrl = filter_input(INPUT_GET, "breweryUrl", FILTER_VALIDATE_INT);
+
+	// Handle all restful calls
+	// GET some of all breweries
 	if($method === "GET") {
 		//set XSRF cookie
-		setXsrfCookie();
+		setXsrfCookie("/");
+		// Determine if a Key was sent by checking $id. If so, pull the requested brewery and update reply
+		if(empty($id) === false) {
 
-		// Get a specific brewery or all breweries and update reply
-		if(empty($breweryId) === false) {
+
+			
 			$brewery = Edu\Cnm\BrewCrew\Brewery::getBreweryByBreweryId($pdo, $breweryId);
 			if($brewery !== null) {
 				$reply->data = $brewery;
@@ -54,10 +74,12 @@ try {
 				$reply->data = $breweries;
 			}
 		}
+		// PUT and POST
 	} else if($method === "PUT" || $method === "POST") {
 
 		verifyXsrf();
 		$requestContent = file_get_contents("php://input");
+		// Is this the correct file path?
 		$requestObject = json_decode($requestContent);
 
 		// Make sure brewery content is available
