42e11d550691731d5aebb28f2612e6177e0c2a7b
McGaughey <therealmcgaughey@gmail.com>
2016-05-30 15:49:56 -0600

    Copied user api content out of index api file and into user api file
    https://app.asana.com/0/117435667927162/118836678397151

--- public_html/php/apis/user/index.php
+++ public_html/php/apis/user/index.php
@@ -1,135 +1,154 @@
 <?php
-
-require_once dirname(__DIR__, 2) . "/classes/autoload.php";
-require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
+require_once dirname(dirname(__DIR__)) . "/classes/autoloader.php";
+require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once dirname(dirname(__DIR__)) . "/lib/sendEmail.php";
 
 use Edu\Cnm\BrewCrew;
 
-
 /**
- * api for the User class
+ * API for user class
  *
- * @author Arlene Carol Graham <agraham14@cnm.edu>
- **/
+ * @author Kate McGaughey therealmcgaughey@gmail.com
+ *
+ * @see https://github.com/CreativeCorrie/time-crunchers/blob/master/public_html/php/api/user/index.php for api I copied
+ */
 
-//verify the session, start if not active
+// Verify xsrf challenge
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
-
-//prepare an empty reply
+// Prepare a empty reply
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 
 try {
-	//grab the mySQL connection
+	// Grab the MySQL connection
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/brewcrew.ini");
 
-	//determine which HTTP method was used
+	// Determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	$reply->method=$method;
 
-	//sanitize input
+	// Sanitize inputs
 	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 
-	//make sure the id is valid for methods that require it
+	// Make sure the id is valid for methods that require it
 	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
-		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+		throw(new InvalidArgumentException("id cannot be negative or empty", 405));
 	}
 
-	// handle GET request - if id is present, that user is returned, otherwise all users are returned
-	if($method === "GET") {
-		//set XSRF cookie
-		setXsrfCookie();
+	// Sanitize and trim other fields
+	$userId = filter_input(INPUT_GET, "userId", FILTER_VALIDATE_INT);
+	$userBreweryId = filter_input(INPUT_GET, "userBreweryId", FILTER_VALIDATE_INT);
+	$userAccessLevel = filter_input(INPUT_GET, "userAccessLevel", FILTER_VALIDATE_INT);
+	$userActivationToken = filter_input(INPUT_GET, "userActivationToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$userEmail = filter_input(INPUT_GET, "userEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$userUsername = filter_input(INPUT_GET, "userUsername", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
-		//get a specific user or all users and update reply
+	// Handle rest calls, while only allowing admins to access database-modifying methods
+	if($method === "GET") {
+		// Set XSRF cookie
+		setXsrfCookie("/");
+
+		// Get the user based on the given
 		if(empty($id) === false) {
-			$user = BrewCrew\User::getUserByUserId($pdo, $id);
+			$user = User::getUserByUserId($pdo, $id);
 			if($user !== null) {
 				$reply->data = $user;
 			}
-		}  else {
-			$users = BrewCrew\User::getAllUsers($pdo);
-			if($users !== null) {
-				$reply->data = $users;
+		} else if(empty($userBreweryId) === false) {
+			$user = User::getUserByUserBreweryId($pdo, $userBreweryId);
+			if($user !== null) {
+				$reply->data = $user;
 			}
+		} else if(empty($userAccessLevel) === false) {
+			$user = User::getUserByUserAccessLevel($pdo, $userAccessLevel);
+			if($user !== null) {
+				$reply->data = $user;
+			}
+		} else if(empty($userActivationToken) === false) {
+			$user = User::getUserByUserActivation($pdo, $userActivationToken);
+			if($user !== null) {
+				$reply->data = $user;
+			}
+		} else if(empty($userEmail) === false) {
+			$user = User::getUserByUserEmail($pdo, $userEmail);
+			if($user !== null) {
+				$reply->data = $user;
+			}
+		} else if(empty($userUsername) === false) {
+			$user = User::getUserByUserUsername($pdo, $userUsername);
+			if($user !== null) {
+				$reply->data = $user;
+			}
+		} else {
+			$users = User::getAllUsers($pdo);
+			$reply->data = $users;
 		}
-	} else if($method === "PUT" || $method === "POST") {
 
+	} else if($method === "PUT") {
 		verifyXsrf();
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
 
-		//make sure user content is available
-		if(empty($requestObject->userContent) === true) {
-			throw(new \InvalidArgumentException ("No content for User.", 405));
+		// Make sure all fields are present in order to prevent database issues
+		if(empty($requestObject->userBreweryId) === true) {
+			throw(new InvalidArgumentException ("userBreweryId cannot be empty", 405));
 		}
+		$requestObject->userEmail = (filter_var($requestObject->userEmail, FILTER_SANITIZE_EMAIL));
+		$requestObject->userFirstName = (filter_var($requestObject->userFirstName, FILTER_SANITIZE_STRING));
+		$requestObject->userLastName = (filter_var($requestObject->userLastName, FILTER_SANITIZE_STRING));
+		$requestObject->userUsername = (filter_var($requestObject->userUsername, FILTER_SANITIZE_STRING));
 
-
-		//perform the actual put or post
-		if($method === "PUT") {
-
-			// retrieve the user to update
-			$user = BrewCrew\User::getUserByUserId($pdo, $id);
-			if($user === null) {
-				throw(new RuntimeException("User does not exist", 404));
-			}
-
-			// put the new user content into the user and update
-			$user->setUserContent($requestObject->userContent);
-			$user->update($pdo);
-
-			// update reply
-			$reply->message = "User updated OK";
-
-		} else if($method === "POST") {
-
-			//  make sure userId is available
-			if(empty($requestObject->userId) === true) {
-				throw(new \InvalidArgumentException ("No User ID.", 405));
-			}
-
-			// create new user and insert into the database
-			$user = new BrewCrew\User(null, $requestObject->userId, $requestObject->userContent, null);
-			$user->insert($pdo);
-
-			// update reply
-			$reply->message = "User created OK";
-		}
-
-	} else if($method === "DELETE") {
-		verifyXsrf();
-
-		// retrieve the User to be deleted
-		$user = BrewCrew\User::getUserByUserId($pdo, $id);
+		// Perform the actual put
+		$user = User::getUserByUserId($pdo, $id);
 		if($user === null) {
-			throw(new RuntimeException("User does not exist", 404));
+			throw(new RuntimeException("User does not exist.", 404));
 		}
 
-		// delete user
-		$user->delete($pdo);
+		// If there's a password, hash it and set it
+		if(isset($requestObject->userPassword) === true && isset($requestObject->confirmPassword) === true) {
+			if($requestObject->userPassword !== $requestObject->confirmPassword) {
+				throw (new \RangeException("Passwords do not match."));
+			} else {
+				$hash = hash_pbkdf2("sha512", $requestObject->userPassword, $user->getUserSalt(), 262144);
+				$user->setUserHash($hash);
+			}
+		}
+		$user->setUserEmail($requestObject->userEmail);
+		$user->setUserFirstName($requestObject->userFirstName);
+		$user->setUserLastName($requestObject->userLastName);
+		$user->setUserUsername($requestObject->userUsername);
+		$user->update($pdo);
+		$reply->message="User updated successfully.";
 
-		// update reply
-		$reply->message = "User deleted OK";
-	} else {
-		throw (new InvalidArgumentException("Invalid HTTP method request"));
+	} else if ($method === "DELETE") {
+		$reply->debug="Delete started.";
+		$user = User::getUserByUserId($pdo, $id);
+		if($user === null) {
+			throw(new RuntimeException("User does not exist.", 404));
+		}
+		$user->delete($pdo);
+		$deletedObject = new stdClass();
 	}
 
-	// update reply with exception information
+	header("Content-type: application/json");
+	json_encode($reply);
+	unset($reply->salt);
+	unset($reply->hash);
+	echo $reply;
+
 } catch(Exception $exception) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
-	$reply->trace = $exception->getTraceAsString();
-} catch(TypeError $typeError) {
+	header("Content-type: application/json");
+	echo (json_encode($reply));
+
+} catch(\TypeError $typeError) {
 	$reply->status = $typeError->getCode();
 	$reply->message = $typeError->getMessage();
+	header("Content-type: application/json");
+	echo (json_encode($reply));
 }
-
-header("Content-type: application/json");
-if($reply->data === null) {
-	unset($reply->data);
-}
-
-// encode and return reply to front end caller
-echo json_encode($reply);
