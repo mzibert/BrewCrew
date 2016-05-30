d2bc9c5dc3e3448ed88b30afa08ed3ce5f58c621
merri zibert <mzibert@cnm.edu>
2016-05-30 13:13:16 -0600

    https://app.asana.com/0/117435667927162/118836678397144 working on Api

---
+++ public_html/php/apis/signout/index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
+require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\BrewCrew;
+
+
+/**
+ * api for signout
+ *
+ * @author Merri J Zibert <mjzibert2@gmail.com>
+ **/
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/brewcrew.ini");
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
---
+++ public_html/php/apis/signup/index.php
@@ -0,0 +1,104 @@
+<?php
+
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
+require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\BrewCrew;
+
+
+/**
+ * api for signup
+ *
+ * @author Merri J Zibert <mjzibert2@gmail.com>
+ **/
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/brewcrew.ini");
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	if($method === "POST") {
+
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		if(empty($requestObject->userFirstName) === true) {
+			throw(new \InvalidArgumentException ("Must fill in first name", 405));
+		} else {
+			$userFirstName = filter_var($requestObject->userFirstName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		}
+
+
+	if(empty($requestObject->userLastName) === true) {
+			throw(new \InvalidArgumentException ("Must fill in valid last name", 405));
+		} else {
+		$userLastName = filter_var($requestObject->userLastName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	}
+
+		if(empty($requestObject->userUserName) === true) {
+			throw(new \InvalidArgumentException ("Must input valid user name", 405));
+		} else {
+			$userUserName = filter_var($requestObject->userUserName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		}
+
+		if(empty($requestObject->password) === true) {
+			throw(new \InvalidArgumentException ("Must input valid password", 405));
+		} else {
+			$password = filter_var($requestObject->password, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		}
+		
+		if(empty($requestObject->userDateOfBirth) === true) {
+			throw(new \InvalidArgumentException ("Must fill in valid Date of Birth", 405));
+		} else {
+			$userDateofBirth = filter_var($requestObject->userDateOfBirth, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		}
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}catch(\Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(\TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
