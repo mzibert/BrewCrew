2eb7c7a65224c54f5701b8754af183c8fd1f3953
McGaughey <therealmcgaughey@gmail.com>
2016-05-31 10:57:39 -0600

    Tweaked user api
    https://app.asana.com/0/117435667927162/118836678397151

--- public_html/php/apis/user/index.php
+++ public_html/php/apis/user/index.php
@@ -1,8 +1,8 @@
 <?php
+
 require_once dirname(dirname(__DIR__)) . "/classes/autoloader.php";
 require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-require_once dirname(dirname(__DIR__)) . "/lib/sendEmail.php";
 
 use Edu\Cnm\BrewCrew;
 
