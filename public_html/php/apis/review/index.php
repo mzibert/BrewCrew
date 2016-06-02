fbd1df19223c28a82e802473697a479764e2f242
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-06-02 16:12:28 -0600

    changes to error messages, adding 401s to exceptions
    https://app.asana.com/0/117435667927162/118836678397150

--- public_html/php/apis/review/index.php
+++ public_html/php/apis/review/index.php
@@ -4,7 +4,7 @@ require_once dirname(__DIR__, 2) . "/classes/autoload.php";
 require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-use Edu\Cnm\BrewCrew;
+use Edu\Cnm\BrewCrew\ReviewTag;
 
 /**
  *
