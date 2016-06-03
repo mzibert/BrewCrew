1ebac84d87e8a854d89ec820316d924e23e47956
McGaughey <therealmcgaughey@gmail.com>
2016-06-03 10:37:32 -0600

    Changed exception on line 75 to multiple exceptions for different attributes
    https://app.asana.com/0/117435667927162/118836678397151

--- public_html/php/apis/user/index.php
+++ public_html/php/apis/user/index.php
@@ -71,13 +71,22 @@ try {
 		$requestObject = json_decode($requestContent);
 
 		// Make sure all fields are present in order to prevent database issues
-		if(empty($requestObject->userBreweryId) === true) {
-			throw(new InvalidArgumentException ("userBreweryId cannot be empty", 405));
-		}
 		$requestObject->userEmail = (filter_var($requestObject->userEmail, FILTER_SANITIZE_EMAIL));
 		$requestObject->userFirstName = (filter_var($requestObject->userFirstName, FILTER_SANITIZE_STRING));
 		$requestObject->userLastName = (filter_var($requestObject->userLastName, FILTER_SANITIZE_STRING));
 		$requestObject->userUsername = (filter_var($requestObject->userUsername, FILTER_SANITIZE_STRING));
+		if(empty($requestObject->userEmail) === true) {
+			throw(new InvalidArgumentException ("userEmail cannot be empty", 405));
+		}
+		if(empty($requestObject->userFirstName) === true) {
+			throw(new InvalidArgumentException ("userFirstName cannot be empty", 405));
+		}
+		if(empty($requestObject->userLastName) === true) {
+			throw(new InvalidArgumentException ("userLastName cannot be empty", 405));
+		}
+		if(empty($requestObject->userUsername) === true) {
+			throw(new InvalidArgumentException ("userUsername cannot be empty", 405));
+		}
 
 		// Perform the actual put
 		$user = BrewCrew\User::getUserByUserId($pdo, $id);
