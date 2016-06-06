dd789d0c2b693126140c7aaa03372c2520839141
Arlene Graham <agraham14@cnm.edu>
2016-06-06 16:36:46 -0600

    added non working contact form https://app.asana.com/0/117435671931146/118830311351347

---
+++ public_html/contact/index.php
@@ -0,0 +1,94 @@
+<?php
+/*grab current directory*/
+$CURRENT_DIR = __DIR__;
+
+/*set page title here*/
+$PAGE_TITLE = "Contact Us";
+
+/*load the HTML head tag: head-utils.php*/
+require_once(dirname(__DIR__) . "/php/partials/head-utils.php");
+?>
+<body class="sfooter content-layout">
+	<div class="sfooter-content">
+
+		<!-- insert header and navbar -->
+		<?php require_once(dirname(__DIR__) . "/php/partials/header.php");?>
+
+		<!-- begin main content page layout -->
+		<main class="container p-t-nav">
+
+
+
+			<!-- begin main content area -->
+			<div class="row row-flex content-wrap">
+
+				<!-- page content -->
+				<div class="col-sm-9 col-sm-push-3 content-panel">
+					<div>
+						<!--Begin Contact Form-->
+						<form id="contact-form" action="<?php echo $PREFIX;?>php/mailer.php" method="post">
+							<div class="form-group">
+								<label for="name">Name <span class="text-danger">*</span></label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-user" aria-hidden="true"></i>
+									</div>
+									<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email">Email <span class="text-danger">*</span></label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-envelope" aria-hidden="true"></i>
+									</div>
+									<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="subject">Subject</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-pencil" aria-hidden="true"></i>
+									</div>
+									<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="message">Message <span class="text-danger">*</span></label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-comment" aria-hidden="true"></i>
+									</div>
+									<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
+								</div>
+							</div>
+
+							<!-- reCAPTCHA -->
+							<div class="g-recaptcha" data-sitekey="6Lc7ox8TAAAAAN9sf714pyF4Kjp1CkOHg3AUMVYr"></div>
+
+							<button class="btn btn-info" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+							<button class="btn btn-default" type="reset"><i class="fa fa-ban"></i> Reset</button>
+						</form>
+
+						<!--empty area for form error/success output-->
+						<div class="row">
+							<div class="col-xs-12">
+								<div id="output-area"></div>
+							</div>
+						</div>
+					</div><!--/.content-panel-->
+				</div><!--/.content-wrap-->
+
+
+
+			</div><!--/.row-flex-->
+		</main>
+
+	</div><!--/.sfooter-content-->
+
+	<!-- insert footer -->
+	<?php require_once(dirname(__DIR__) . "/php/partials/footer.php");?>
+
+</body>
+</html>
