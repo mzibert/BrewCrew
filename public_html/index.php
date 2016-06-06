a50dbeb79f8083dabe76e6e19d01e8142258cdb0
Arlene Graham <agraham14@cnm.edu>
2016-06-06 14:53:26 -0600

    added text and images https://app.asana.com/0/117435671931146/118830311351347

--- public_html/index.php
+++ public_html/index.php
@@ -58,10 +58,12 @@
 						<nav>
 							<ul class="nav nav-pills pull-right">
 								<li role="presentation" class="active"><a href="#">Home</a></li>
-								<li role="presentation"><a href="#">About</a></li>
-								<li role="presentation"><a href="#">Contact</a></li>
+								<li role="presentation"><a href="#">The Compass</a></li>
 								<li role="presentation"><a href="#">Beer</a></li>
 								<li role="presentation"><a href="#">Breweries</a></li>
+								<li role="presentation"><a href="#">Sign Up</a></li>
+								<li role="presentation"><a href="#">Login</a></li>
+								<<li role="presentation"><a href="#">Contact</a></li>
 							</ul>
 						</nav>
 						<h3 class="text-muted">Project name</h3>
@@ -71,9 +73,9 @@
 				<!-- Main jumbotron for a primary marketing message or call to action -->
 				<div class="jumbotron">
 					<div class="container">
-						<h1>Hello, world!</h1>
+						<h1>ABQ Brew Crew!</h1>
 						<p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-						<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+						<p><a class="btn btn-primary btn-lg" href="#" role="button">Join Us! &raquo;</a></p>
 					</div>
 				</div>
 
@@ -81,27 +83,58 @@
 					<!-- Example row of columns -->
 					<div class="row">
 						<div class="col-md-4">
-							<h2>Heading</h2>
+							<h2>Most Reviewed Beers</h2>
 							<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
 							<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 						</div>
 						<div class="col-md-4">
-							<h2>Heading</h2>
+							<h2>Heading....</h2>
 							<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
 							<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 						</div>
 						<div class="col-md-4">
-							<h2>Heading</h2>
+							<h2>Most ....</h2>
 							<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
 							<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 						</div>
 					</div>
 
 					<hr>
-
+					<div class="container well-lg" id="beercompass">
+						<p>BEER COMPASS</p>
+						</div>
 
 				</main>
-				<!-- FOOTER -->
+			<br><br><hr><br>
+			<h2 class="heading h-section text-center" data-barley="index_hiw_heading" data-barley-editor="simple">Here’s how it works</h2>
+			<div class="row">
+				<div class="col-lg-10 col-lg-offset-1">
+					<div class="row">
+						<section class="box-choose hiw col-sm-4">
+							<figure><div data-barley="idx_hiw_step_1_img" data-barley-editor="image" data-width="97" data-height="103"><img src="image/beer1.jpg" alt=""></div></figure>
+							<h4 class="subheading" data-barley="idx_hiw_step_1_text" data-barley-editor="simple">1. Join ABQ Beer Crew</h4>
+							<p>We make it really easy! Sign up in a single step, and it's simple to cancel anytime.</p>
+						</section>
+						<section class="box-choose hiw col-sm-4">
+							<figure><div data-barley="idx_hiw_step_2_img" data-barley-editor="image" data-width="197" data-height="120"><img src="image/beer2.jpg" alt=""></div></figure>
+							<h4 class="subheading" data-barley="idx_hiw_step_2_text" data-barley-editor="simple">2. Create Your Flavor Profile</h4>
+							<p>Text Text Text</p>
+						</section>
+						<section class="box-choose hiw col-sm-4">
+							<figure><div data-barley="idx_hiw_step_3_img" data-barley-editor="image" data-width="139" data-height="120"><img src="image/cerveza-buena.jpg" alt=""></div></figure>
+							<h4 class="subheading" data-barley="idx_hiw_step_3_text" data-barley-editor="simple">3. Get Recomendations</h4>
+							<p>More Text.</p>
+						</section>
+					</div>
+					<br>
+					<div class="row">
+						<div class="col-lg-4 col-lg-offset-4 col-sm-4 col-sm-offset-4 col-xs-8 col-xs-offset-2">
+							<a href="/subscribe/46573752_calmbox?gift=False" data-barley="content_cta_main" data-barley-editor="linkedit" class="btn btn-primary fullsize mb-15">Start Today</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- FOOTER -->
 				<footer>
 
 					<div class="col-md-12">
