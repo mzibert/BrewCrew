7a84e17e61c2b8d48bf46f7cd26a3248860d7581
Arlene Graham <agraham14@cnm.edu>
2016-06-08 17:06:13 -0600

    added bootstrap links back into head-utils.php because without them the nav bar does not work. https://app.asana.com/0/117435667927162/118836678397143

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -17,7 +17,7 @@
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<li role="presentation" class="active"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/public_html/">Home <span class="sr-only">(current)</span></a></li>
-					<li><a href="#">Link</a></li>
+
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">The Beer Compass<span class="caret"></span></a>
 						<ul class="dropdown-menu">
@@ -38,19 +38,8 @@
 					</div>
 					<button type="submit" class="btn btn-default">Submit</button>
 				</form>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#">Link</a></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="#">Action</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li role="separator" class="divider"></li>
-							<li><a href="#">Separated link</a></li>
-						</ul>
-					</li>
-				</ul>
+
+
 			</div><!-- /.navbar-collapse -->
 		</div><!-- /.container-fluid -->
 	</nav>
