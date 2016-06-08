1bd561759922b5f779059683b6b9aaa84c000114
Arlene Graham <agraham14@cnm.edu>
2016-06-08 07:58:03 -0600

    added toggle to header.php https://app.asana.com/0/117435667927162/118836678397143

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -3,6 +3,17 @@
 	<div class="container">
 		<div class="header clearfix">
 			<nav>
+				div class="navbar-wrapper">
+				<div class="container">
+					<nav class="navbar navbar-inverse navbar-static-top">
+						<div class="container">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+						  data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
 				<ul class="nav nav-pills pull-right">
 					<li role="presentation" class="active"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/public_html/">Home</a></li>
 					<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/thecompass/">The Compass</a></li>
@@ -12,7 +23,10 @@
 					<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/signin/">Sign In</a></li>
 					<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/contact/">Contact</a></li>
 				</ul>
+						</div>
+				</div>
 			</nav>
+
 			<h3 class="text-muted">ABQ Brew Crew</h3>
 		</div>
 </header>
