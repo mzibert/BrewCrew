ec9493f37041566d4e1bf455f4b7cc72feefb2da
Arlene Graham <agraham14@cnm.edu>
2016-06-08 08:05:22 -0600

    added toggle to header.php https://app.asana.com/0/117435667927162/118836678397143

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -1,32 +1,40 @@
 <header>
 
-	<div class="container">
-		<div class="header clearfix">
-			<nav>
-				div class="navbar-wrapper">
+
+			<div class="navbar-wrapper">
 				<div class="container">
 					<nav class="navbar navbar-inverse navbar-static-top">
 						<div class="container">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-						  data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<ul class="nav nav-pills pull-right">
-					<li role="presentation" class="active"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/public_html/">Home</a></li>
-					<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/thecompass/">The Compass</a></li>
-					<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/beer/">Beer</a></li>
-					<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/breweries/">Breweries</a></li>
-					<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/signup/">Sign Up</a></li>
-					<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/signin/">Sign In</a></li>
-					<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/contact/">Contact</a></li>
-				</ul>
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+										  data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+
+							</div>
+							<div id="navbar" class="navbar-collapse collapse">
+								<ul class="nav navbar-nav">
+								<li role="presentation" class="active"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/public_html/">Home</a></li>
+								<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/thecompass/">TheCompass</a></li>
+								<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/beer/">Beer</a></li>
+								<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/breweries/">Breweries</a></li>
+								<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/signup/">SignUp</a></li>
+								<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/signin/">SignIn</a></li>
+								<li role="presentation"><a href="https://bootcamp-coders.cnm.edu/~agraham14/BrewCrew/public_html/contact/">Contact</a></li>
+								</ul>
+
+
+							</div>
 						</div>
-				</div>
-			</nav>
+					</nav>
 
 			<h3 class="text-muted">ABQ Brew Crew</h3>
+		</div>
 		</div>
 </header>
