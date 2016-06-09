edca4fedf12edc6ea477f139d85faa5fbf47522b
merri zibert <mzibert@cnm.edu>
2016-06-09 07:52:35 -0600

    https://app.asana.com/0/117435667927162/118836678397146 added begining to signout

--- public_html/php/apis/signout/index.php
+++ public_html/php/apis/signout/index.php
@@ -13,6 +13,20 @@ use Edu\Cnm\BrewCrew;
  * @author Merri J Zibert <mjzibert2@gmail.com>
  **/
 
+//prepare default error message
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/brewcrew.ini");
+
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+
 if($method === "GET"){
 	if(session_status() !== PHP_SESSION_ACTIVE) {
 		session_start();
