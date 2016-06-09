8aaba40e3bc8e3cd4d68edfc8a55c30d94242d2b
McGaughey <therealmcgaughey@gmail.com>
2016-06-09 15:53:49 -0600

    updated file paths with one more (dirname
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/thecompass.php
+++ public_html/angular/templates/thecompass.php
@@ -2,19 +2,19 @@
 
 
 /*load the HTML head tag: head-utils.php*/
-require_once(dirname(__DIR__) . "/php/partials/head-utils.php");
+require_once(dirname(dirname(__DIR__)) . "/php/partials/head-utils.php");
 ?>
 <body class="sfooter content-layout">
 	<div class="sfooter-content">
 
 		<!-- insert header and navbar -->
-		<?php require_once(dirname(__DIR__) . "/php/partials/header.php");?>
+		<?php require_once(dirname(dirname(__DIR__)) . "/php/partials/header.php");?>
 
 		<!-- begin main content page layout -->
 		<main class="container p-t-nav">
 
 			<!-- insert the page title up top -->
-			<?php require_once(dirname(__DIR__) . "/php/partials/page-title.php");?>
+			<?php require_once(dirname(dirname(__DIR__)) . "/php/partials/page-title.php");?>
 
 			<!-- begin main content area -->
 			<div class="row row-flex content-wrap">
@@ -34,6 +34,6 @@ require_once(dirname(__DIR__) . "/php/partials/head-utils.php");
 	</div><!--/.sfooter-content-->
 
 	<!-- insert footer -->
-	<?php require_once(dirname(__DIR__) . "/php/partials/footer.php");?>
+	<?php require_once(dirname(dirname(__DIR__)) . "/php/partials/footer.php");?>
 
 </body>
