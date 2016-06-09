0ac82cee4a5ab994a45cca39fd6e8b8f1eb010c2
Arlene Graham <agraham14@cnm.edu>
2016-06-09 15:29:07 -0600

    added navController to header
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -1,5 +1,5 @@
-<header>
-
+<header ng-controller="navController">
+	<bootstrap-breakpoint></bootstrap-breakpoint>
 	<nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<!-- Brand and toggle get grouped for better mobile display -->
