d8075294736277de8de1dfcb55ddd9b8f9e11342
Arlene Graham <agraham14@cnm.edu>
2016-06-09 15:18:42 -0600

    refined the footer
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -1,9 +1,12 @@
 <!-- FOOTER -->
 <footer>
 
-	<div class="col-md-12">
-
+	<footer class="p-y-4">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 m-b-2">
 		<p>&copy; The ABQ Brew Crew 2016 &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
 	</div>
-
+</div>
+</div>
 </footer>
