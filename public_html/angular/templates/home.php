7ea992e0a2f18a9205b632907e47a81d6f7f8f95
McGaughey <therealmcgaughey@gmail.com>
2016-06-10 11:02:49 -0600

    matched content on lower half of page to wireframe
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/home.php
+++ public_html/angular/templates/home.php
@@ -1,96 +1,93 @@
 <main>
-<div class="col-xs-12">
-	<div class="jumbotron">
+	<div class="col-xs-12">
+		<div class="jumbotron">
+			<div class="container">
+				<p>We're tracking x beers from x craft breweries in Albuquerque!</p>
+				<p><a class="btn btn-primary btn-lg" href="#" role="button">Join Us! &raquo;</a></p>
+			</div>
+		</div>
+
 		<div class="container">
-			<p>We're tracking x beers from x craft breweries in Albuquerque!</p>
-			<p><a class="btn btn-primary btn-lg" href="#" role="button">Join Us! &raquo;</a></p>
-		</div>
-	</div>
+			<!-- Example row of columns -->
+			<div class="row">
+				<div class="col-md-4">
+					<h2>Most Reviewed Beers</h2>
+					<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+						mauris
+						condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+						euismod. Donec sed odio dui. </p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div>
+				<div class="col-md-4">
+					<h2>Heading....</h2>
+					<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+						mauris
+						condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+						euismod. Donec sed odio dui. </p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div>
+				<div class="col-md-4">
+					<h2>Most ....</h2>
+					<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
+						porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
+						ut
+						fermentum massa justo sit amet risus.</p>
+					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				</div>
+			</div>
 
-	<div class="container">
-		<!-- Example row of columns -->
-		<div class="row">
-			<div class="col-md-4">
-				<h2>Most Reviewed Beers</h2>
-				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-					condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
-					euismod. Donec sed odio dui. </p>
-				<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-			</div>
-			<div class="col-md-4">
-				<h2>Heading....</h2>
-				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-					condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
-					euismod. Donec sed odio dui. </p>
-				<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-			</div>
-			<div class="col-md-4">
-				<h2>Most ....</h2>
-				<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
-					porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut
-					fermentum massa justo sit amet risus.</p>
-				<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-			</div>
+			<hr>
+			<!--		<div class="container well-lg" id="beercompass">-->
+			<!--			<p>BEER COMPASS</p>-->
+			<!--		</div>-->
 		</div>
-
-		<hr>
-		<div class="container well-lg" id="beercompass">
-			<p>BEER COMPASS</p>
-		</div>
-	</div>
 </main>
 <br><br>
-<hr>
 <br>
-<h2 class="heading h-section text-center" data-barley="index_hiw_heading" data-barley-editor="simple">Here’s how it
-	works</h2>
-<div class="container">
-	<div class="row col-md-4">
 
-
-		<img src="image/beer1.jpg" alt="">
-
-		<h4>1. Join ABQ Beer
-			Crew</h4>
-		<p>We make it really easy! Sign up in a single step, and it's simple to cancel anytime.</p>
-	</div>
-	<div class="col-md-4">
-
-
-		<img src="image/beer2.jpg" alt="">
-
-		<h4>2. Create Your
-			Flavor Profile</h4>
-		<p>Text Text Text</p>
-	</div>
-	<div class="col-md-4">
-
-
-		<img src="image/cerveza-buena.jpg" alt="">
-
-		<h4>3. Get
-			Recomendations</h4>
-		<p>More Text.</p>
-	</div>
-
-	<br>
-	<!-- page content -->
-	<div class="row">
-		<div class="col-md-12">
-			<h1>Let's get started!</h1>
-		</div>
-	</div>
-
-	<div class="row">
-		<div class="col-md-6">
-			<!-- sign up button -->
-			<h4>Sign Up to Create Your Flavor Profile</h4>
-			<br>
-			<div class="button-container">
-				<a href="signup.php" class="btn btn-lg btn-success">Sign Up</a>
+<div class="row">
+	<div class="span9">
+		<h2 class="heading h-section text-center" data-barley="index_hiw_heading" data-barley-editor="simple">Start by Joining and Rating Beers</h2>
+		<div class="row">
+			<div class="col-xs-12">
+				<img src="image/beer1.jpg" alt="placeholder for snapshot of reviews">
 			</div>
 		</div>
+	</div>
 
+	<div class="span9">
+		<h2 class="heading h-section text-center" data-barley="index_hiw_heading" data-barley-editor="simple">Generate Your Flavor Profile</h2>
+		<div class="row">
+			<div class="col-xs-12">
+				<img src="image/beer1.jpg" alt="placeholder for snapshot of user's flavor profile">
+			</div>
+		</div>
+	</div>
+
+	<div class="span9">
+		<h2 class="heading h-section text-center" data-barley="index_hiw_heading" data-barley-editor="simple">Get the Best Recommendations</h2>
+		<div class="row">
+			<div class="col-xs-12">
+				<img src="image/reviews.png" alt="placeholder for snapshot of compass search results">
+			</div>
+		</div>
+	</div>
+
+	<div>
+		<h4>Join Us!</h4>
+		<p>We make it really easy! Sign up in a single step, and cancel anytime.</p>
+		<div class="row">
+			<div class="col-xs-12">
+				<!-- sign up button -->
+				<h4>Sign Up to Create Your Flavor Profile</h4>
+				<br>
+				<div class="button-container">
+					<a href="signup.php" class="btn btn-lg btn-success">Sign Up</a>
+				</div>
+			</div>
+	</div>
+
+	<div>
 		<!-- Account Holder controls-->
 		<h4>Already Have an Account?</h4>
 		<div class="col-md-6">
@@ -133,7 +130,4 @@
 		<div class="col-md-12">
 			<a href="#">Forgot Password?</a>
 		</div>
-
-
 	</div>
-</div>
