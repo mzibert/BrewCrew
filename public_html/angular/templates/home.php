cb209836bc1806de2f8dd4e2d29451e9293520cf
McGaughey <therealmcgaughey@gmail.com>
2016-06-10 09:56:29 -0600

    removed head utilities in beer template
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/home.php
+++ public_html/angular/templates/home.php
@@ -2,10 +2,7 @@
 
 	<div class="jumbotron">
 		<div class="container">
-			<h1>ABQ Brew Crew!</h1>
-			<p>This is a template for a simple marketing or informational website. It includes a large callout called a
-				jumbotron and three supporting pieces of content. Use it as a starting point to create something more
-				unique.</p>
+			<p>We're tracking x beers from x craft breweries in Albuquerque!</p>
 			<p><a class="btn btn-primary btn-lg" href="#" role="button">Join Us! &raquo;</a></p>
 		</div>
 	</div>
