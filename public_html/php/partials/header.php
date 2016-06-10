a6c99426757e8051f3dc58cddfae0a06132996cc
Arlene Graham <agraham14@cnm.edu>
2016-06-09 21:48:49 -0600

    added directive to head-utlis
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -1,10 +1,10 @@
 <header ng-controller="navController">
+
 	<bootstrap-breakpoint></bootstrap-breakpoint>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="container">
+		<nav class="navbar navbar-inverse">
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" ng-click="navCollapsed = !navCollapsed">
 					<span class="sr-only">Toggle navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
