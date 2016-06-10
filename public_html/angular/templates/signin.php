d6c17bb3c70729b1d48ca7886953275eded06274
McGaughey <therealmcgaughey@gmail.com>
2016-06-10 12:28:49 -0600

    started working on modal
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/signin.php
+++ public_html/angular/templates/signin.php
@@ -1,27 +1,27 @@
 
 
-			<div class="container colored" >
-				<div class="col-md-4"></div>
-				<div class="col-md-4">
-					<div class="container" style="margin-top: 30px">
-						<div class="col-md-4">
-							<form>
-								<h3>Log In</h3>
-								<label for="Email" class="sr-only">Email </label>
-								<input type="email" id="Email" class="form-control" placeholder="Email" required="required" autofocus="autofocus"/>
-								<label for="pwd" class="sr-only">Password</label>
-								<input type="password" id="pwd1" class="form-control" placeholder="Password" required="required" />
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" value="remember-me"/>Stay Logged</label></div>
-								<button class="btn btn-sm btn-primary btn-block" type="submit">Sign in</button>
-							</form>
-						</div>
-					</div>
+<div class="container colored" >
+	<div class="col-md-4"></div>
+	<div class="col-md-4">
+		<div class="container" style="margin-top: 30px">
+			<div class="col-md-4">
+				<form>
+					<h3>Log In</h3>
+					<label for="Email" class="sr-only">Email </label>
+					<input type="email" id="Email" class="form-control" placeholder="Email" required="required" autofocus="autofocus"/>
+					<label for="pwd" class="sr-only">Password</label>
+					<input type="password" id="pwd1" class="form-control" placeholder="Password" required="required" />
+					<div class="checkbox">
+						<label>
+							<input type="checkbox" value="remember-me"/>Stay Logged</label></div>
+					<button class="btn btn-sm btn-primary btn-block" type="submit">Sign in</button>
+				</form>
+			</div>
+		</div>
 
 
 
-			<
+		<
 	</div><!--/.sfooter-content-->
 
 
