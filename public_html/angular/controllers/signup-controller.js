89fbcb16cb7bd4373930f24f394a53d8550f0f08
Arlene Graham <agraham14@cnm.edu>
2016-06-10 12:24:30 -0600

    added boilerplate to beer-controller
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/controllers/signup-controller.js
+++ public_html/angular/controllers/signup-controller.js
@@ -15,7 +15,7 @@ app.controller("signupController", ["$scope", "SignupService", "$location", func
 				.then(function(result) {
 					if(result.data.status === 200) {
 						$scope.alerts[0] = {type: "success", msg: result.data.message};
-						$location.url("signup/home.php");
+						$location.url("signup/");
 					} else {
 						$scope.alerts[0] = {type: "danger", msg: result.data.message};
 					}
