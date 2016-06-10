480c8956cbd13a3d882b43345122c028d6dc86be
Kate McGaughey <kmcgaughey@cnm.edu>
2016-06-10 17:35:57 -0600

    tweaked layout of home
    https://app.asana.com/0/117435667927162/list

--- public_html/angular/templates/home.php
+++ public_html/angular/templates/home.php
@@ -1,129 +1,119 @@
-<main>
-	<div class="col-xs-12">
-		<div class="jumbotron">
-			<div class="container">
-				<p>We're tracking x beers from x craft breweries in New Mexico!</p>
-				<p><a class="btn btn-primary btn-lg" href="#" role="button">Join Us! &raquo;</a></p>
-			</div>
-		</div>
-		
+<div class="col-xs-12">
+	<div class="jumbotron">
 		<div class="container">
-			<!-- Example row of columns -->
+			<p class="lead">We're tracking x beers from 50 craft breweries in New Mexico!</p>
+			<p><a class="btn btn-primary btn-lg" href="#" role="button">Join Us! &raquo;</a></p>
+		</div>
+	</div>
+	<!-- Featured beers, breweries, and reviewers -->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4">
+				<h3>Most Popular Breweries</h3>
+				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+					mauris
+					condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+					euismod. Donec sed odio dui. </p>
+				<p><a class="btn btn-default" href="#" role="button">See more &raquo;</a></p>
+			</div>
+			<div class="col-md-4">
+				<h3>Top Rated Beers</h3>
+				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+					mauris
+					condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
+					euismod. Donec sed odio dui. </p>
+				<p><a class="btn btn-default" href="#" role="button">See more &raquo;</a></p>
+			</div>
+			<div class="col-md-4">
+				<h3>Most Prolific Reviewers</h3>
+				<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
+					porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
+					ut
+					fermentum massa justo sit amet risus.</p>
+				<p><a class="btn btn-default" href="#" role="button">See more &raquo;</a></p>
+			</div>
+		</div>
+	</div>
+
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="span9">
+			<h2 class="heading h-section text-center" data-barley="index_hiw_heading" data-barley-editor="simple">Start by Joining and Rating Beers</h2>
 			<div class="row">
-				<div class="col-md-4">
-					<h2>Most Popular Breweries</h2>
-					<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-						mauris
-						condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
-						euismod. Donec sed odio dui. </p>
-					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-				</div>
-				<div class="col-md-4">
-					<h2>Top Rated Beers</h2>
-					<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-						mauris
-						condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis
-						euismod. Donec sed odio dui. </p>
-					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-				</div>
-				<div class="col-md-4">
-					<h2>Most Prolific Reviewers</h2>
-					<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
-						porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
-						ut
-						fermentum massa justo sit amet risus.</p>
-					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+				<div class="col-xs-12">
+					<img src="image/reviews.png" alt="placeholder for snapshot of reviews">
 				</div>
 			</div>
-
-			<hr>
-			<!--		<div class="container well-lg" id="beercompass">-->
-			<!--			<p>BEER COMPASS</p>-->
-			<!--		</div>-->
 		</div>
-</main>
-<br><br>
-<br>
 
-<div class="row">
-	<div class="span9">
-		<h2 class="heading h-section text-center" data-barley="index_hiw_heading" data-barley-editor="simple">Start by Joining and Rating Beers</h2>
-		<div class="row">
-			<div class="col-xs-12">
-				<img src="image/reviews.png" alt="placeholder for snapshot of reviews">
+		<div class="span9">
+			<h2 class="heading h-section text-center" data-barley="index_hiw_heading" data-barley-editor="simple">Generate Your Flavor Profile</h2>
+			<div class="row">
+				<div class="col-xs-12">
+					<img src="image/beer1.jpg" alt="placeholder for snapshot of user's flavor profile">
+				</div>
+			</div>
+		</div>
+
+		<div class="span9">
+			<h2 class="heading h-section text-center" data-barley="index_hiw_heading" data-barley-editor="simple">Get the Best Recommendations</h2>
+			<div class="row">
+				<div class="col-xs-12">
+					<img src="image/beer2.jpg" alt="placeholder for snapshot of compass search results">
+				</div>
 			</div>
 		</div>
 	</div>
+</div>
 
-	<div class="span9">
-		<h2 class="heading h-section text-center" data-barley="index_hiw_heading" data-barley-editor="simple">Generate Your Flavor Profile</h2>
-		<div class="row">
-			<div class="col-xs-12">
-				<img src="image/beer1.jpg" alt="placeholder for snapshot of user's flavor profile">
-			</div>
-		</div>
-	</div>
-
-	<div class="span9">
-		<h2 class="heading h-section text-center" data-barley="index_hiw_heading" data-barley-editor="simple">Get the Best Recommendations</h2>
-		<div class="row">
-			<div class="col-xs-12">
-				<img src="image/beer2.jpg" alt="placeholder for snapshot of compass search results">
-			</div>
-		</div>
-	</div>
-
-	<div>
-		<h4>Join Us!</h4>
-		<p>We make it really easy! Sign up in a single step, and cancel anytime.</p>
-		<div class="row">
-			<div class="col-xs-12">
-				<!-- sign up button -->
-				<h4>Sign Up to Create Your Flavor Profile</h4>
-				<br>
-				<div class="button-container">
-					<a href="signup.php" class="btn btn-lg btn-success">Sign Up</a>
-				</div>
-			</div>
-	</div>
-
-	<div>
-		<!-- Account Holder controls-->
-		<h4>Already Have an Account?</h4>
-		<div class="col-md-6">
-			<!-- Log in modal trigger button -->
-			<br>
+<div>
+	<h1>Join Us!</h1>
+	<p>Sign up in a single step, and cancel anytime.</p>
+	<div class="row">
+		<div class="col-xs-12">
+			<!-- sign up button -->
+			<h4>Sign Up to Create Your Flavor Profile</h4>
 			<div class="button-container">
-				<a href="signin.php" class="btn btn-lg btn-success">
-					Log In
-				</a>
+				<a href="signup.php" class="btn btn-lg btn-success">Sign Up</a>
 			</div>
-		</div> <!-- end of column-->
+		</div>
+	</div>
+<div>
+	<!-- Account Holder controls-->
+	<h4>Already Have an Account?</h4>
+	<div class="col-md-6">
+		<!-- Log in modal trigger button -->
+		<br>
+		<div class="button-container">
+			<a href="signin.php" class="btn btn-lg btn-success">Log In</a>
+		</div>
+	</div> <!-- end of column-->
 
-		<!--Log In Modal -->
-		<div class="modal fade" id="modal-lg" tabindex="-1" role="dialog" aria-labelledby="largeModalLabel"
-			  aria-hidden="true" data-keyboard="true">
-			<!-- Modals have two optional sizes, available via class="modal-lg" or class="modal-sm" -->
-			<div class="modal-dialog modal-lg">
-				<!-- Begin modal content here -->
-				<div class="modal-content">
-					<div class="modal-header">
-						<!-- close button -->
-						<button type="button" class="close" data-dismiss="modal" aria-label="close">
-							<span aria-hidden="true">×</span>
-						</button>
-						<h3 class="modal-title">ABQ Brew Crew</h3>
-					</div>
-					<div class="modal-body">
-						<label for="modalLoginForm" class="control-label">Enter your email address and password
-							here</label>
-					</div>
+	<!--Log In Modal -->
+	<div class="modal fade" id="modal-lg" tabindex="-1" role="dialog" aria-labelledby="largeModalLabel"
+		  aria-hidden="true" data-keyboard="true">
+		<!-- Modals have two optional sizes, available via class="modal-lg" or class="modal-sm" -->
+		<div class="modal-dialog modal-lg">
+			<!-- Begin modal content here -->
+			<div class="modal-content">
+				<div class="modal-header">
+					<!-- close button -->
+					<button type="button" class="close" data-dismiss="modal" aria-label="close">
+						<span aria-hidden="true">×</span>
+					</button>
+					<h3 class="modal-title">ABQ Brew Crew</h3>
+				</div>
+				<div class="modal-body">
+					<label for="modalLoginForm" class="control-label">Enter your email address and password
+						here</label>
 				</div>
 			</div>
-		</div> <!-- End of Log in modal-->
+		</div>
+	</div> <!-- End of Log in modal-->
 
 
-	</div> <!--end of row-->
+</div> <!--end of row-->
 
 
 	<div class="row">
