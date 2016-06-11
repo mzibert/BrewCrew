7c85f304b96a7304aead187e6236f13fb6bbac7f
merri zibert <mzibert@cnm.edu>
2016-06-11 15:44:46 -0600

    https://app.asana.com/0/117435667927162/118836678397143 Wrote service for breweries

--- public_html/angular/services/breweries-service.js
+++ public_html/angular/services/breweries-service.js
@@ -1,3 +1,45 @@
 /**
  * Created by chiwe_000 on 6/9/2016.
  */
+
+app.constant("BREWERY_ENDPOINT", "php/apis/brewery/");
+app.service("breweryService", function($http, BREWERY_ENDPOINT) {
+
+	function getUrl() {
+		return(BREWERY_ENDPOINT);
+	}
+
+	function getUrlForId(breweryId) {
+		return(getUrl() + breweryId);
+	}
+
+	this.all = function() {
+		return($http.get(getUrl()));
+	};
+
+	this.fetchBreweryById = function(breweryId) {
+		return($http.get(getUrlForId() + scheduleId));
+	};
+
+	this.fetchBreweryByLocation = function(breweryLocation) {
+		return($http.get(getUrl() + "?breweryLocation=" + breweryLocation));
+	};
+
+	this.fetchBreweryByName = function(breweryName) {
+		return($http.get(getUrl() + "?breweryName=" + breweryName));
+	};
+
+
+	this.update = function(breweryId, brewery) {
+		return($http.put(getUrlForId(breweryId, brewery)));
+	};
+
+	this.create = function(brewery) {
+		return($http.post(getUrl(), brewery));
+	};
+
+	this.destroy = function(breweryId) {
+		return($http.delete(getUrlForId(breweryhId)));
+	};
+});
+
