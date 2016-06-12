a2d2639b2e3fba679c463046434ed7e24a3d5383
Arlene Graham <agraham14@cnm.edu>
2016-06-12 13:31:37 -0600

    finished review service https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/services/review-service.js
+++ public_html/angular/services/review-service.js
@@ -1,3 +1,46 @@
-/**
- * Created by chiwe_000 on 6/12/2016.
- */
+app.constant("ACTIVATION_ENDPOINT", "php/api/review/");
+app.service("ReviewService", function($http, ACTIVATION_ENDPOINT) {
+
+	function getUrl() {
+		return(ACTIVATION_ENDPOINT);
+	}
+
+	function getUrlForReviewId(reviewId) {
+		return(getUrl() + reviewId);
+	}
+
+	this.fetchReviewByReviewId = function(reviewId) {
+		return($http.get(getUrlForId(reviewId)));
+	};
+
+	this.fetchReviewByReviewBeerId = function(reviewBeerId) {
+		return($http.get(getUrl()+ "?reviewBeerId=" + reviewBeerId));
+	};
+
+	this.fetchReviewByReviewUserId = function(reviewUserId) {
+		return($http.get(getUrl()+ "?reviewUserId=" + reviewUserId));
+	};
+	this.fetchReviewByReviewDate = function(reviewDate) {
+		return($http.get(getUrl()+ "?reviewDate=" + reviewDate));
+	};
+	this.fetchReviewByReviewPintRating = function(reviewPintRating) {
+		return($http.get(getUrl()+ "?reviewPintRating=" + reviewPintRating));
+	};
+
+
+	this.fetchAllReviews = function() {
+		return($http.get(getUrl()));
+	};
+
+	this.update = function(reviewId, review) {
+		return($http.put(getUrlForReviewId(reviewId, review)));
+	};
+
+	this.create = function(review) {
+		return($http.post(getUrl(), review));
+	};
+
+	this.destroy = function(reviewId) {
+		return($http.delete(getUrlForReviewId(reviewId)));
+	}
+});
