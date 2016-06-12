f3271644f5b59b5a64a5f113c0ae59474c028325
Arlene Graham <agraham14@cnm.edu>
2016-06-12 12:48:59 -0600

    started review service and controller https://app.asana.com/0/117435667927162/118836678397143

---
+++ public_html/angular/controllers/review-controller.js
@@ -0,0 +1,13 @@
+app.controller('userController', ["$scope", "userService", function($scope, userService) {
+	$scope.alerts = [];
+	$scope.userData = [];
+
+	$scope.getUserById = function() {
+		userService.fetchUserById(userId)
+			.then(function(result) {
+				if(result.status.data === 200) {
+					$scope.data = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
