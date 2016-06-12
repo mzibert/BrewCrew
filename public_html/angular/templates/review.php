c798dbe6cae1811d9fd3e4efe7acbc701b0380cb
Arlene Graham <agraham14@cnm.edu>
2016-06-12 13:45:09 -0600

    added temporary review form in review.php https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/review.php
+++ public_html/angular/templates/review.php
@@ -1,7 +1,46 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: chiwe_000
- * Date: 6/12/2016
- * Time: 12:54 PM
- */
+<form>
+	<fieldset class="form-group">
+		<label for="exampleInputEmail1">Email address</label>
+		<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+		<small class="text-muted">We'll never share your email with anyone else.</small>
+	</fieldset>
+	<fieldset class="form-group">
+		<label for="exampleInputPassword1">Password</label>
+		<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+	</fieldset>
+
+	<fieldset class="form-group">
+		<label for="exampleTextarea">Write your review here.</label>
+		<textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+	</fieldset>
+	<fieldset class="form-group">
+		<label for="exampleInputFile">Share a picture.</label>
+		<input type="file" class="form-control-file" id="exampleInputFile">
+		<small class="text-muted">Select your Pint Rating 1 - 5. 1 being the worst and 5 being the best.</small>
+	</fieldset>
+	<div class="radio">
+		<label>
+			<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+			1 Pint Worst Beer in the Joint
+		</label>
+		<label>
+			<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+			2 Pints Worst Beer in the Joint's prettier sister
+		</label>
+		<label>
+			<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+			3 Pints Not too bad
+		</label>
+		<label>
+			<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+			4 Pints I would drink it again
+		</label>
+		<label>
+			<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+			5 Pints Damn near perfection
+		</label>
+
+	</div>
+
+	<button type="submit" class="btn btn-primary">Submit</button>
+</form>
