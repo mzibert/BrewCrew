0e8c56e3713c54c575d4d5a8ad7a3644b9971f28
Arlene Graham <agraham14@cnm.edu>
2016-06-12 13:12:01 -0600

    finished review controller https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/controllers/review-controller.js
+++ public_html/angular/controllers/review-controller.js
@@ -1,9 +1,9 @@
-app.controller('ReviewController', ["$scope", "ReviewService", function($scope, userService) {
+app.controller('ReviewController', ["$scope", "ReviewService", function($scope, ReviewService) {
 	$scope.alerts = [];
 	$scope.userData = [];
 
-	$scope.getUserById = function() {
-		userService.fetchUserById(userId)
+	$scope.getReviewById = function() {
+		ReviewService.fetchReviewByReviewId(reviewId)
 			.then(function(result) {
 				if(result.status.data === 200) {
 					$scope.data = result.data.data;
@@ -11,3 +11,56 @@ app.controller('ReviewController', ["$scope", "ReviewService", function($scope, 
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
 			})
+	};
+	$scope.getReviewBeerId = function() {
+		ReviewService.fetchReviewBeerId(reviewBeerId)
+			.then(function(result) {
+				if(result.status.data === 200) {
+					$scope.data = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+
+	$scope.getReviewUserId = function() {
+		ReviewService.fetchReviewUserId(reviewBeerId)
+			.then(function(result) {
+				if(result.status.data === 200) {
+					$scope.data = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+	$scope.getReviewUserId = function() {
+		ReviewService.fetchReviewUserId(reviewUserId)
+			.then(function(result) {
+				if(result.status.data === 200) {
+					$scope.data = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+	$scope.getReviewDate = function() {
+		ReviewService.fetchReviewDate(reviewDate)
+			.then(function(result) {
+				if(result.status.data === 200) {
+					$scope.data = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+	$scope.getReviewPintRating = function() {
+		ReviewService.fetchReviewPintRatingl(reviewPintRating)
+			.then(function(result) {
+				if(result.status.data === 200) {
+					$scope.data = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+	]
