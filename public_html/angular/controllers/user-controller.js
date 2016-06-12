056f1717588e1c155ba9269ccf4eeb5b95608ba2
Arlene Graham <agraham14@cnm.edu>
2016-06-12 12:12:41 -0600

    updated user controller https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/controllers/user-controller.js
+++ public_html/angular/controllers/user-controller.js
@@ -1,3 +1,50 @@
-/**
- * Created by chiwe_000 on 6/12/2016.
- */
+app.controller('UserController', ["$scope", "userService", function($scope, userService) {
+	$scope.alerts = [];
+	$scope.userData = [];
+
+	$scope.getUserById = function() {
+		userService.fetchUserById(userId)
+			.then(function(result) {
+				if(result.status.data === 200) {
+					$scope.data = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+
+	$scope.getUserByActivation = function() {
+		userService.fetchUserByActivation(userActivation)
+			.then(function(result) {
+				if(result.status.data === 200) {
+					$scope.data = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+
+
+	$scope.getUserByUserDateOfBirth = function(){
+		userService.fetchUserByUserDateOfBirth(userDateOfBirth)
+			.then(function(result){
+				if(result.status.data === 200){
+					$scope.data = result.data.data;
+				}else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+	$scope.getUserByUserEmail = function() {
+		userService.fecthUserByUserEmail(userEmail)
+			.then(function(result) {
+				if(rsult.status.data === 200) {
+					$scope.data = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+
+
+]
--- public_html/angular/templates/user.php
+++ public_html/angular/templates/user.php
@@ -1,7 +1,66 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: chiwe_000
- * Date: 6/12/2016
- * Time: 10:53 AM
- */
+<div class="container" id="profile">
+	<h1 class="page-header">Edit Profile</h1>
+	<div class="row">
+		<!-- left column -->
+		<div class="col-md-4 col-sm-6 col-xs-12">
+			<div class="text-center">
+				<img src="http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg" class="avatar img-circle img-thumbnail" alt="avatar">
+				<h6>Upload a different photo...</h6>
+				<input type="file" class="text-center center-block well well-sm">
+			</div>
+		</div>
+		<!-- edit form column -->
+		<div class="col-md-8 col-sm-6 col-xs-12 personal-info">
+
+			<h3>Personal info</h3>
+			<form class="form-horizontal" role="form">
+				<div class="form-group">
+					<label class="col-lg-3 control-label">First name:</label>
+					<div class="col-lg-8">
+						<input class="form-control" value="Jane" type="text">
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-lg-3 control-label">Last name:</label>
+					<div class="col-lg-8">
+						<input class="form-control" value="Bishop" type="text">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label class="col-lg-3 control-label">Email:</label>
+					<div class="col-lg-8">
+						<input class="form-control" value="email@gmail.com" type="text">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label class="col-md-3 control-label">Username:</label>
+					<div class="col-md-8">
+						<input class="form-control" value="janeuser" type="text">
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-md-3 control-label">Password:</label>
+					<div class="col-md-8">
+						<input class="form-control" value="11111122333" type="password">
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-md-3 control-label">Confirm password:</label>
+					<div class="col-md-8">
+						<input class="form-control" value="11111122333" type="password">
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-md-3 control-label"></label>
+					<div class="col-md-8">
+						<input class="btn btn-primary" value="Save Changes" type="button">
+						<span></span>
+						<input class="btn btn-default" value="Cancel" type="reset">
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
