3cec1591f794ecd964a94dc8751796d742d3deb0
Arlene Graham <agraham14@cnm.edu>
2016-06-12 13:22:55 -0600

    finished review controller https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/controllers/review-controller.js
+++ public_html/angular/controllers/review-controller.js
@@ -54,7 +54,7 @@ app.controller('ReviewController', ["$scope", "ReviewService", function($scope, 
 			})
 	};
 	$scope.getReviewPintRating = function() {
-		ReviewService.fetchReviewPintRatingl(reviewPintRating)
+		ReviewService.fetchReviewPintRating(reviewPintRating)
 			.then(function(result) {
 				if(result.status.data === 200) {
 					$scope.data = result.data.data;
@@ -63,4 +63,53 @@ app.controller('ReviewController', ["$scope", "ReviewService", function($scope, 
 				}
 			})
 	};
-	]
+	$scope.getAllReviews = function() {
+		ReviewService.fetchAllReviews()
+			.then(function(result) {
+				if(result.status.data === 200) {
+					$scope.data = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+	/**
+	 * creates a review and sends it to the review API
+	 *
+	 * @param review
+	 * @param validated true if Angular validated the form, false if not
+	 **/
+	$scope.createReview = function(review, validated) {
+		if(validated === true) {
+			ReviewService.create(review)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+						$scope.newReview = {reviewId: null, reviewBeerId: null, reviewUserId: null, reviewDate: "", reviewPintRating: null, reviewText: ""};
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
+	};
+
+	/**
+	 * updates a review and sends it to the review API
+	 *
+	 * @param review
+	 * @param validated true if Angular validated the form, false if not
+	 **/
+	$scope.updateReview= function(review, validated) {
+		if(validated === true) {
+			ReviewService.update(review.reviewId, review)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
+	};
+
+}]);
