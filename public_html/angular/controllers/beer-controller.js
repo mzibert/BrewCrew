9300e5c462a5be3241a6b1f727baa569b5b493f6
merri zibert <mzibert@cnm.edu>
2016-06-12 13:43:57 -0600

    working on beer controller

--- public_html/angular/controllers/beer-controller.js
+++ public_html/angular/controllers/beer-controller.js
@@ -1,3 +1,72 @@
-app.controller('BeerController', function($scope) {
+app.controller('beerController', ["$scope", "beerservice", function($scope, beerService) {
+
+
+	$scope.getBeerById = function() {
+		beerService.fetchBeerById(beerId)
+			.then(function(result) {
+
+				
+			})
+		
+	}
 
 });
+
+	$scope.getBeerByBreweryId = function() {
+		beerService.fetchBeerByBreweryId(breweryId)
+			.then(function(result) {
+
+
+			})
+
+	}
+
+});
+
+	$scope.getBeerByIbu = function() {
+		beerService.fetchBeerByIbu(beerIbu)
+			.then(function(result) {
+
+
+		})
+
+}
+
+});
+
+	$scope.getBeerByColor = function() {
+		beerService.fetchBeerByColor(beerColor)
+			.then(function(result) {
+
+
+		})
+
+}
+
+});
+
+	$scope.getBeerByName = function() {
+		beerService.fetchBeerByName(beerName)
+			.then(function(result) {
+
+
+		})
+
+}
+
+});
+
+$scope.getBeerByColor = function() {
+	beerService.fetchBeerByColor(beerColor)
+		.then(function(result) {
+
+
+		})
+
+}
+
+});
+
+/**Do we need all beers???
+ **/
+
