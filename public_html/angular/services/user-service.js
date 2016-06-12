3dfc9168679843862b239db376dd3030aac1698a
Arlene Graham <agraham14@cnm.edu>
2016-06-12 12:46:02 -0600

    added user-service.js https://app.asana.com/0/117435667927162/118836678397143

---
+++ public_html/angular/services/user-service.js
@@ -0,0 +1,52 @@
+app.constant("ACTIVATION_ENDPOINT", "php/api/user/");
+app.service("userService", function($http, ACTIVATION_ENDPOINT) {
+
+	function getUrl() {
+		return(ACTIVATION_ENDPOINT);
+	}
+
+	function getUrlForId(userId) {
+		return(getUrl() + userId);
+	}
+
+	this.fetchUserByUserId = function(userId) {
+		return($http.get(getUrlForId(userId)));
+	};
+	this.fetchUserByUserActivationToken = function(userActivationToken) {
+		return($http.get(getUrl()+ "?userActivationToken=" + userActivationToken));
+	};
+
+	this.fetchUserByUserDateOfBirth = function(userDateOfBirth) {
+		return($http.get(getUrl()+ "?userDateOfBirth=" + userDateOfBirth));
+	};
+
+	this.fetchUserByUserEmail = function(userEmail) {
+		return($http.get(getUrl()+ "?userEmail=" + userEmail));
+	};
+
+	this.fetchUserByUserFirstName = function(userFirstName) {
+		return($http.get(getUrl()+ "?userFirstName=" + userFirstName));
+	};
+	this.fetchUserByUserLastName = function(userLastName) {
+		return($http.get(getUrl()+ "?userLastName=" + userLastName));
+	};
+	this.fetchUserByUserUsername = function(userUsername) {
+		return($http.get(getUrl()+ "?userUsername=" + userUsername));
+	};
+
+	this.fetchAllUsers = function() {
+		return($http.get(getUrl()));
+	};
+
+	this.update = function(userId, user) {
+		return($http.put(getUrlForUserId(userId, user)));
+	};
+
+	this.create = function(user) {
+		return($http.post(getUrl(), user));
+	};
+
+	this.destroy = function(userId) {
+		return($http.delete(getUrlForUserId(userId)));
+	}
+});
