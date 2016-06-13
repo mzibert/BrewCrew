8d680bbb47a96b9570f1ebc802c9cd489cbf99a2
McGaughey <therealmcgaughey@gmail.com>
2016-06-13 13:00:45 -0600

    Worked on footer
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -3,7 +3,7 @@
 	<div class="container-fluid">
 		<div class="row">
 			<img align="right" class="acknowledgement" src="image/poweredbybrewerydb.png" alt="Powered by BreweryDB"/>
-			<p>&copy; The ABQ Brew Crew 2016 &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+			<p>&copy; Craft Beer Compass 2016 &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
 		</div>
 	</div>
 </footer>
