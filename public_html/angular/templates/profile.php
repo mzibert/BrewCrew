445ac7641843dad69fc15e0d2b8e6c2e3bc9f5df
McGaughey <therealmcgaughey@gmail.com>
2016-06-12 23:27:24 -0600

    created template for user page
    https://app.asana.com/0/117435667927162/118836678397143

---
+++ public_html/angular/templates/profile.php
@@ -0,0 +1,47 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-9">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-md-12 lead">User profile<hr></div>
+					</div>
+					<div class="row">
+						<div class="col-md-4 text-center">
+							<img class="img-thumbnail profile-image" style="-webkit-user-select:none;
+              display:block; margin:auto;" src="image/profile-placeholder.jpg">
+						</div>
+						<div class="col-md-8">
+							<div class="row">
+								<div class="col-md-12">
+									<h1 class="profile-information">Username</h1>
+									<h3 class="profile-information">First Name Last Name</h3>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-6">
+									<span class="text-muted">Joined: 01.01.2016</span><br>
+									<span class="text-muted">Birthday:01.01.1970</span><br>
+									<span class="text-muted">Gender:</span>?<br><br>
+								</div>
+								<div class="col-md-6">
+									<div class="activity-mini">
+										<i class="glyphicon glyphicon-comment text-muted"></i> 34
+									</div>
+									<div class="activity-mini">
+										<i class="glyphicon glyphicon-thumbs-up text-muted"></i> 17
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<hr><button class="btn btn-default pull-right"><i class="glyphicon glyphicon-pencil"></i> Edit</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
