f3d55c2a76a2ca86f5a36f2fa94a94708da6c8ed
McGaughey <therealmcgaughey@gmail.com>
2016-06-12 23:49:14 -0600

    created template for brewery profile page
    https://app.asana.com/0/117435667927162/118836678397143

---
+++ public_html/angular/templates/breweryprofile.php
@@ -0,0 +1,34 @@
+<div class="container">
+	<div class="brewery-profile">
+		<img align="left" class="brewery-image" src="image/cerveza-buena.jpg" alt="Profile placeholder"/>
+		<img align="left" class="image-logo thumbnail" src="image/profile-placeholder.jpg" alt="brewery logo placeholder"/>
+		<div class="profile-text">
+			<h1>Albuquerque Brewery</h1>
+			<p>Est. Date</p>
+			<p>Contact Information</p>
+			<p>Brewery Description</p>
+		</div>
+		<div>
+			<div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h3 class="panel-title">Top Rated Beers</h3>
+								</div>
+								<ul class="list-group beers">
+									<a href="#" class="list-group-item">Cras justo odio</a>
+									<a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+									<a href="#" class="list-group-item">Morbi leo risus</a>
+									<a href="#" class="list-group-item">Porta ac consectetur ac</a>
+									<a href="#" class="list-group-item">Vestibulum at eros</a>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div> 
