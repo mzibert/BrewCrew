0c4532a3f08d1da9d398ddf1989f14d172441888
Arlene Graham <agraham14@cnm.edu>
2016-06-13 12:06:55 -0600

    corrected review-controller
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/controllers/review-controller.js
+++ public_html/angular/controllers/review-controller.js
@@ -33,16 +33,7 @@ app.controller('ReviewController', ["$scope", "ReviewService", function($scope, 
 				}
 			})
 	};
-	$scope.getReviewUserId = function() {
-		ReviewService.fetchReviewUserId(reviewUserId)
-			.then(function(result) {
-				if(result.status.data === 200) {
-					$scope.data = result.data.data;
-				} else {
-					$scope.alerts[0] = {type: "danger", msg: result.data.message};
-				}
-			})
-	};
+
 	$scope.getReviewDate = function() {
 		ReviewService.fetchReviewDate(reviewDate)
 			.then(function(result) {
