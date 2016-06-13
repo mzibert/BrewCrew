8f293663887176ce8a54fea121ed9caa7561600c
McGaughey <therealmcgaughey@gmail.com>
2016-06-12 23:57:14 -0600

    worked on template for beer profile page
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/beerprofile.php
+++ public_html/angular/templates/beerprofile.php
@@ -4,6 +4,16 @@
 		<img align="left" class="image-profile thumbnail" src="image/profile-placeholder.jpg" alt="beer profile placeholder"/>
 		<div class="profile-text">
 			<h1>Some Beer</h1>
+<!--			rating script-->
+			<div ng-controller="RatingDemoCtrl">
+				<h4>Default</h4>
+				<uib-rating ng-model="rate" max="max" read-only="isReadonly" on-hover="hoveringOver(value)" on-leave="overStar = null" titles="['one','two','three']" aria-labelledby="default-rating"></uib-rating>
+				<span class="label" ng-class="{'label-warning': percent<30, 'label-info': percent>=30 && percent<70, 'label-success': percent>=70}" ng-show="overStar && !isReadonly">{{percent}}%</span>
+
+				<pre style="margin:15px 0;">Rate: <b>{{rate}}</b> - Readonly is: <i>{{isReadonly}}</i> - Hovering over: <b>{{overStar || "none"}}</b></pre>
+
+				<hr />
+			</div>
 			<h3>Beer Awards</h3>
 			<h3>Beer Abv</h3>
 			<h3>Beer Ibu</h3>
