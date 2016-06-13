56a1f6530b2639ae314a2c4af858aa1accb808c5
McGaughey <therealmcgaughey@gmail.com>
2016-06-13 10:55:24 -0600

    added 'powered by breweryDB' to footer
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -1,12 +1,13 @@
 <!-- FOOTER -->
 <footer>
 
-	<footer class="p-y-4">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 m-b-2">
-		<p>&copy; The ABQ Brew Crew 2016 &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+<footer class="p-y-4">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 m-b-2">
+			<p>&copy; The ABQ Brew Crew 2016 &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+			</div>
+		</div>
+		<img align="right" class="acknowledgement" src="image/poweredbybrewerydb.png" alt="Powered by BreweryDB"/>
 	</div>
-</div>
-</div>
 </footer>
