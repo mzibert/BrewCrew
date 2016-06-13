48cd46e2e426487a177286bab7bcea420d2362eb
McGaughey <therealmcgaughey@gmail.com>
2016-06-12 23:44:23 -0600

    worked on template for beer profile page
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/beerprofile.php
+++ public_html/angular/templates/beerprofile.php
@@ -1,14 +1,35 @@
 <div class="container">
 	<div class="beer-profile">
 		<img align="left" class="beer-image" src="image/cerveza-buena.jpg" alt="Profile placeholder"/>
-		<img align="left" class="image-profile thumbnail" src="image/profile-placeholder.jpg" alt="Profile placeholder"/>
+		<img align="left" class="image-profile thumbnail" src="image/profile-placeholder.jpg" alt="beer profile placeholder"/>
 		<div class="profile-text">
-			<h1>Albuquerque Brewery</h1>
-			<p>Est. Date</p>
-			<p>Brewery Description</p>
+			<h1>Some Beer</h1>
+			<h3>Beer Awards</h3>
+			<h3>Beer Abv</h3>
+			<h3>Beer Ibu</h3>
+			<p>Beer Description</p>
 		</div>
 		<div>
-<!--			sidebar for brewery contact info?-->
+			<div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h3 class="panel-title">Recent Reviews</h3>
+								</div>
+								<ul class="list-group beers">
+									<a href="#" class="list-group-item">Cras justo odio</a>
+									<a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+									<a href="#" class="list-group-item">Morbi leo risus</a>
+									<a href="#" class="list-group-item">Porta ac consectetur ac</a>
+									<a href="#" class="list-group-item">Vestibulum at eros</a>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div> 
