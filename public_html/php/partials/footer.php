3996f82131682596279ef4541224df39cdd3a43b
merri zibert <mzibert@cnm.edu>
2016-06-13 15:15:56 -0600

    https://app.asana.com/0/117435667927162/118836678397143 Added privacy template, still trying to get the link to work

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -3,7 +3,7 @@
 	<div class="container-fluid">
 		<div class="row">
 			<img align="right" class="acknowledgement" src="image/poweredbybrewerydb.png" alt="Powered by BreweryDB"/>
-			<p class="text-muted">&copy; Craft Beer Compass 2016 &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+			<p class="text-muted">&copy; Craft Beer Compass 2016 &middot; <a href="privacy">Privacy</a> &middot; <a href="#">Terms</a></p>
 		</div>
 	</div>
 </footer>
