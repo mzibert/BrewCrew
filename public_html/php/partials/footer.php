00bc95eb1276f0e89c17d1e634900057cbb2bc6a
McGaughey <therealmcgaughey@gmail.com>
2016-06-13 13:13:24 -0600

    Worked on footer
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -1,9 +1,9 @@
 <!--Footer-->
-<footer class="footer">
+<footer class="footer fixed-bottom">
 	<div class="container-fluid">
 		<div class="row">
 			<img align="right" class="acknowledgement" src="image/poweredbybrewerydb.png" alt="Powered by BreweryDB"/>
-			<p>&copy; Craft Beer Compass 2016 &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+			<p class="text-muted">&copy; Craft Beer Compass 2016 &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
 		</div>
 	</div>
 </footer>
