597c29f5c3525a0b76e516ab953784fe99e430e1
McGaughey <therealmcgaughey@gmail.com>
2016-06-14 02:56:27 -0600

    trying to fix routeparameters for breweryprofile
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/controllers/beerprofile-controller.js
+++ public_html/angular/controllers/beerprofile-controller.js
@@ -1,22 +1,27 @@
-app.controller("BeerProfileController", ["$routeParams", "$scope", "$uibModal", "$window", "BeerProfileService", function($routeParams, $scope, $uibModal, $window, BeerProfileService) {
-	$scope.deletedBeerProfile = false;
-	$scope.beerprofile = null;
+app.controller("BeerProfileController", ["$routeParams", "$scope", "BeerProfileService", function($routeParams, $scope, BeerProfileService) {
+	$scope.beerProfile = null;
 	$scope.alerts = [];
 	$scope.beerData = [];
 
 	$scope.getBeerProfile = function() {
 		BeerProfileService.fetch($routeParams.id)
 			.then(function(result) {
+				console.log(result);
 				if(result.data.status === 200) {
-					if(result.data.data !== undefined) {
-						$scope.beerprofile = result.data.data;
-						$scope.deletedBeerprofile = false;
+					$scope.beerprofile = result.data.data;
 					} else {
-						$scope.alerts[0] = {type: "warning", msg: "Beerprofile " + $routeParams.id + " has been deleted"};
-						$scope.deletedBeerprofile = true;
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
 					}
+				})
+		};
+	$scope.getBreweryById = function(breweryId) {
+		BreweryProfileService.fetch($routeParams.id)
+			.then(function(result) {
+				if(result.status.data === 200) {
+					$scope.breweryData = result.data.data;
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
-			});
+			})
 	};
+}]);
--- public_html/angular/controllers/breweryprofile-controller.js
+++ public_html/angular/controllers/breweryprofile-controller.js
@@ -3,27 +3,30 @@ app.controller("BreweryProfileController", ["$routeParams", "$scope", "BreweryPr
 	$scope.alerts = [];
 	$scope.breweryData = [];
 
-	$scope.getBreweryProfile = function() {
+	$scope.loadBreweryProfile = function() {
 		BreweryProfileService.fetch($routeParams.id)
+			console.log(BreweryProfileService)
 			.then(function(result) {
 				console.log(result);
 				if(result.data.status === 200) {
-					if(result.data.data !== undefined) {
-						$scope.breweryprofile = result.data.data;
-						$scope.deletedBreweryprofile = false;
+					$scope.breweryprofile = result.data.data;
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
+				return BreweryProfileService;
 			});
 	};
-	$scope.getBreweryById = function(breweryId) {
+	$scope.loadBreweryById = function() {
 			BreweryProfileService.fetch($routeParams.id)
+				console.log(BreweryProfileService)
+				BreweryProfileService.loadBreweryProfile()
 			.then(function(result) {
 				if(result.status.data === 200) {
 					$scope.breweryData = result.data.data;
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
+				return BreweryById;
 			})
 	};
 }]);
