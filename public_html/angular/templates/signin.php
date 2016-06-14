9f7c0f6edaea35f4a7611dda22510212965f8b65
McGaughey <therealmcgaughey@gmail.com>
2016-06-14 11:49:15 -0600

    made sign in form layout pretty
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/signin.php
+++ public_html/angular/templates/signin.php
@@ -1,22 +1,21 @@
 
 
 <div class="container colored" id="signin" >
-	<div class="col-md-4"></div>
-	<div class="col-md-4">
-		<div class="container">
-			<div class="col-md-4">
-				<form role="form" name="userSignInForm" ng-submit="login(signInData);" id="userSignInForm" >
-					<h3>Log In</h3>
-					<label for="text" class="sr-only">Username</label>
-					<input type="text" id="userId" ng-model="signInData.userId" class="form-control" placeholder="username" required="required" autofocus="autofocus"/>
-					<label for="pwd" class="sr-only">Password</label>
-					<input type="password" id="password" ng-model="signInData.password" class="form-control" placeholder="Password" required="required" />
-					<div class="checkbox">
-						<label>
-							<input type="checkbox" value="remember-me"/>Stay Logged</label></div>
-					<button class="btn btn-sm btn-primary btn-block" type="submit">Sign in</button>
-				</form>
-			</div>
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			<form role="form" name="userSignInForm" ng-submit="login(signInData);" id="userSignInForm" >
+				<h2>Log In</h2>
+				<label for="text" class="sr-only">Username</label>
+				<input type="text" id="userId" ng-model="signInData.userId" class="form-control" placeholder="username" required="required" autofocus="autofocus"/>
+				<label for="pwd" class="sr-only">Password</label>
+				<input type="password" id="password" ng-model="signInData.password" class="form-control" placeholder="Password" required="required" />
+				<div class="checkbox">
+					<label>
+						<input type="checkbox" value="remember-me"/>Stay Logged</label></div>
+				<button class="btn btn-sm btn-primary btn-block" type="submit">Sign in</button>
+			</form>
 		</div>
+	</div>
+</div>
 
 
