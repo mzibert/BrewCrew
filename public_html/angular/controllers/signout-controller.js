80e73efb754ccfc487c50c9fd27726a96d991169
Arlene Graham <agraham14@cnm.edu>
2016-06-14 09:02:52 -0600

    writing signin-controller
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/controllers/signout-controller.js
+++ public_html/angular/controllers/signout-controller.js
@@ -1,7 +1,8 @@
 app.controller('SignoutController', ["$scope", "SignoutService", function($scope,  SignoutService) {
 	SignoutService.signout()
 		.then(function() {
-
+			$scope.signoutData = [];
+			$scope.alerts = [];
 		});
 }]);
 
