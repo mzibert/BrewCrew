10ca93da9f8f95ce000a977574e8c28f56b13206
McGaughey <therealmcgaughey@gmail.com>
2016-06-14 17:15:53 -0600

    fixed beerprofile!
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/controllers/beerprofile-controller.js
+++ public_html/angular/controllers/beerprofile-controller.js
@@ -1,14 +1,14 @@
-app.controller("BeerProfileController", ["$routeParams", "$scope", "BeerProfileService", function($routeParams, $scope, BeerProfileService) {
+app.controller("BeerProfileController", ["$routeParams", "$scope", "BeerService", function($routeParams, $scope, BeerService) {
 	$scope.beerProfile = null;
 	$scope.alerts = [];
 	$scope.beerData = [];
 
 	$scope.loadBeerProfile = function() {
-		BeerProfileService.fetch($routeParams.id)
+		BeerService.fetchBeerById($routeParams.id)
 			.then(function(result) {
-				console.log(result);
 				if(result.data.status === 200) {
 					$scope.beerProfile = result.data.data;
+					console.log($scope.beerProfile);
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
--- public_html/angular/templates/beerprofile.php
+++ public_html/angular/templates/beerprofile.php
@@ -2,7 +2,13 @@
 	<div class="beer-profile">
 		<img align="left" class="image-logo thumbnail" src="image/profileplaceholder.jpg" alt="beer logo placeholder"/>
 		<div class="profile-text">
-			<h1>{{ beerProfile.beerName }}</h1>
+			<h1>{{ beerProfile[0].beerName }}</h1>
+			<p>Description: {{ beerProfile[0].beerDescription }}</p>
+			<p>Availability: {{ beerProfile[0].beerAvailability }}</p>
+			<p>Style: {{ beerProfile[0].beerStyle }}</p>
+			<p>ABV: {{ beerProfile[0].beerAbv }}</p>
+			<p>IBU: {{ beerProfile[0].beerIbu }}</p>
+			<p>Awards: {{ beerProfile[0].beerAwards }}</p>
 		</div>
 		<div>
 			<div>
@@ -11,60 +17,38 @@
 						<div class="col-md-6">
 							<div class="panel panel-default">
 								<div class="panel-heading">
-									<h3 class="panel-title">Beer Details</h3>
+									<h3 class="panel-title">Recent Reviews</h3>
 								</div>
-								<ul class="list-group beer">
-									<a href="#" class="list-group-item">Name: {{ beerProfile.beerName }}</a>
-									<a href="#" class="list-group-item">Description: {{ beerProfile.beerDescription }}</a>
-									<a href="#" class="list-group-item">Availability: {{ beerProfile.beerAvailability }}</a>
-									<a href="#" class="list-group-item">Style: {{ beerProfile.beerStyle }}</a>
-									<a href="#" class="list-group-item">ABV: {{ beerProfile.beerAbv }}</a>
-									<a href="#" class="list-group-item">IBU: {{ beerProfile.beerIbu }}</a>
-									<a href="#" class="list-group-item">Awards: {{ beerProfile.beerAwards }}</a>
+								<ul class="list-group beers">
+									<a href="#" class="list-group-item">Cras justo odio</a>
+									<a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+									<a href="#" class="list-group-item">Morbi leo risus</a>
+									<a href="#" class="list-group-item">Porta ac consectetur ac</a>
+									<a href="#" class="list-group-item">Vestibulum at eros</a>
 								</ul>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div>
-					<div>
-						<div class="container">
-							<div class="row">
-								<div class="col-md-10 col-md-offset-1">
-									<div class="panel panel-default">
-										<div class="panel-heading">
-											<h3 class="panel-title">Recent Reviews</h3>
-										</div>
-										<ul class="list-group beers">
-											<a href="#" class="list-group-item">Cras justo odio</a>
-											<a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-											<a href="#" class="list-group-item">Morbi leo risus</a>
-											<a href="#" class="list-group-item">Porta ac consectetur ac</a>
-											<a href="#" class="list-group-item">Vestibulum at eros</a>
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-						<!--				<div class="container">-->
-						<!--					<div class="row">-->
-						<!--						<div class="col-md-6">-->
-						<!--							<div class="panel panel-default">-->
-						<!--								<div class="panel-heading">-->
-						<!--									<h3 class="panel-title">Top Rated Beers</h3>-->
-						<!--								</div>-->
-						<!--								<ul class="list-group beers">-->
-						<!--									<a href="#" class="list-group-item">Cras justo odio</a>-->
-						<!--									<a href="#" class="list-group-item">Dapibus ac facilisis in</a>-->
-						<!--									<a href="#" class="list-group-item">Morbi leo risus</a>-->
-						<!--									<a href="#" class="list-group-item">Porta ac consectetur ac</a>-->
-						<!--									<a href="#" class="list-group-item">Vestibulum at eros</a>-->
-						<!--								</ul>-->
-						<!--							</div>-->
-						<!--						</div>-->
-						<!--					</div>-->
-						<!--				</div>-->
-					</div>
-				</div>
+				<!--				<div class="container">-->
+				<!--					<div class="row">-->
+				<!--						<div class="col-md-6">-->
+				<!--							<div class="panel panel-default">-->
+				<!--								<div class="panel-heading">-->
+				<!--									<h3 class="panel-title">Top Rated Beers</h3>-->
+				<!--								</div>-->
+				<!--								<ul class="list-group beers">-->
+				<!--									<a href="#" class="list-group-item">Cras justo odio</a>-->
+				<!--									<a href="#" class="list-group-item">Dapibus ac facilisis in</a>-->
+				<!--									<a href="#" class="list-group-item">Morbi leo risus</a>-->
+				<!--									<a href="#" class="list-group-item">Porta ac consectetur ac</a>-->
+				<!--									<a href="#" class="list-group-item">Vestibulum at eros</a>-->
+				<!--								</ul>-->
+				<!--							</div>-->
+				<!--						</div>-->
+				<!--					</div>-->
+				<!--				</div>-->
 			</div>
 		</div>
+	</div>
+</div>
