62b551369f6d69694691b418df989e054db486d6
McGaughey <therealmcgaughey@gmail.com>
2016-06-15 13:03:31 -0600

    optimized layout of compass page
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/thecompass.php
+++ public_html/angular/templates/thecompass.php
@@ -6,34 +6,28 @@
 		</div>
 	</div>
 </div>
+<br>
 <div class="container">
 	<div class="row">
-		<div class="col-md-2">
-			<button type="button" class="btn btn-generate" ng-required="true" ng-click="getBeerRecommendation()">
-				Generate Flavor Profile
-			</button>
-		</div>
-	</div>
-</div>
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-9">
 			<div class="ibox float-e-margins">
 				<div class="ibox-content">
-
 					<div class="row">
-						<div id="compass-wrap" class="col-sm-4 col-sm-offset-6 col-xs-10 col-xs-offset-1">
+						<div id="compass-wrap" class="col-sm-4 col-sm-offset-2 col-xs-10 col-xs-offset-1">
 							<div class="compass">
 								<?php require_once(dirname(__DIR__, 2)) . "/image/compass.svg";?>
 							</div>
 						</div>
 					</div>
-
 				</div>
 			</div>
-		</div>
+			<div class="col-md-1">
+				<button type="button" class="btn btn-generate" ng-required="true" ng-click="getBeerRecommendation()">
+					Generate Flavor Profile
+				</button>
+			</div>
 	</div>
 </div>
+<br>
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-11">
