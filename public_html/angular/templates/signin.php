57d372dcc1907afdf3bcf0ea79f7084c21d719ab
Arlene Graham <agraham14@cnm.edu>
2016-06-15 07:53:31 -0600

    changing my signin form back https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/signin.php
+++ public_html/angular/templates/signin.php
@@ -5,15 +5,18 @@
 		<div class="col-md-4 col-md-offset-4">
 			<form role="form" name="userSignInForm"  id="userSignInForm"   class="form-horizontal well" ng-submit="signin("signInData, userSignInForm.$valid);" novalidate>
 				<h2>Log In</h2>
+
 				<label for="text" class="sr-only">Username</label>
-				<input type="text" id="userId" ng-model="signInData.userUserName" class="form-control" placeholder="username" required="required" autofocus="autofocus"/>
+
+				<input type="text" id="userName" ng-model="signInData.userName" class="form-control" placeholder="username" required="required" autofocus="autofocus"/>
+
 				<label for="password">Password</label>
 				<div class="input-group">
 					<div class="input-group-addon">
 						<i class="fa fa-key"></i>
 					</div>
 
-					<input type="password" id="password" name="password" class="form-control" placeholder="Password" ng-model="signInData.userPassword" ng-minlength="4" ng-maxlength="32" ng-required="true" />
+					<input type="password" id="userPassword" name="userPassword" class="form-control" placeholder="Password" ng-model="signInData.userPassword" ng-minlength="4" ng-maxlength="32" ng-required="true" />
 				</div>
 				<div class="alert alert-danger" role="alert" ng-messages="userSignInForm.password.$error" ng-if="userSignInForm.password.$touched" ng-hide="userSignInForm.password.$valid">
 					<p ng-message="minlength">Password is too short.</p>
@@ -23,10 +26,10 @@
 				<h1></h1>
 				<button class="btn btn-lg btn-info" type="submit"><i class="fa fa-paper-plane"></i>&nbsp;Submit</button>
 				<hr />
-			</form>
+			</div>
 		</div>
 	</div>
-</div>
-	</form>
+			</form>
+
 
 
