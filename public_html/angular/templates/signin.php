a5ddfbd58780b55d0c5cdc9ef2a236a7c7f65791
Arlene Graham <agraham14@cnm.edu>
2016-06-15 08:22:31 -0600

    pushing signin form https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/signin.php
+++ public_html/angular/templates/signin.php
@@ -3,7 +3,7 @@
 <div class="container colored" id="signin" >
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4">
-			<form role="form" name="userSignInForm"  id="userSignInForm"   class="form-horizontal well" ng-submit="signin("signInData, userSignInForm.$valid);" novalidate>
+			<form role="form" name="userSignInForm"  id="userSignInForm"   class="form-horizontal well" ng-submit="signin(signInData, userSignInForm.$valid);" novalidate>
 				<h2>Log In</h2>
 
 				<label for="text" class="sr-only">Username</label>
