a625db33f837a3676ba615dc3bfb5cfd50d42aa1
McGaughey <therealmcgaughey@gmail.com>
2016-06-15 08:33:41 -0600

    optimized content
    https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/home.php
+++ public_html/angular/templates/home.php
@@ -14,12 +14,12 @@
 		<div class="col-md-4">
 			<h3>Discover Beers and Breweries</h3>
 			<p>Find craft beer and brewery information, then browse what's currently on tap. We make it easy to search and compare choices.</p>
-			<p>Whether you're looking for a tool to help you learn about the complex world of craft beer, or whether your're a connoisseur looking for something specific, we ensure you can always find the craft beer you're looking for.</p>
-			<p><a class="btn btn-default" href="breweries.php" role="button">See more &raquo;</a></p>
+			<p>Whether you're looking for a tool to help you learn about the flavors and styles of craft beer, or your're a connoisseur looking for something specific, we ensure you can always find the craft beer you're looking for.</p>
+			<p><a class="btn btn-default" href="brewery.php" role="button">See more &raquo;</a></p>
 		</div>
 		<div class="col-md-4">
 			<h3>Generate Your Flavor Profile</h3>
-			<p>The craft beer compass is the ultimate tool for getting great recommendations. We are dedicated to helping users navigate the complex and sometimes confusing world of craft beer.</p>
+			<p>The craft beer compass is the ultimate tool for getting great recommendations. We are dedicated to helping users navigate the complex world of craft beer.</p>
 			<p>As you rate, review, and tag, our algorithm learns your likes and dislikes. We get to know the flavors you enjoy, and help you discover new craft beers based on what you've rated highly in the past.</p>
 			<p><a class="btn btn-default" href="beer.php" role="button">See more &raquo;</a></p>
 		</div>
@@ -27,7 +27,7 @@
 			<h3>Share Your Passion</h3>
 			<p>Tell the world what you think. Give each beer a rating on our "5 pint scale," add a thoughtful review, and use tags to plot the flavor of your favorite craft beers on the compass. Rely on honest feedback from other craft beer lovers.</p>
 			<p>Coming soon: Network with other craft beer lovers, follow users with similar tastes, and connect via your mobile device.</p>
-			<p><a class="btn btn-default" href="#" role="button">See more &raquo;</a></p>
+			<p><a class="btn btn-default" href="signup.php" role="button">See more &raquo;</a></p>
 		</div>
 	</div>
 </div>
