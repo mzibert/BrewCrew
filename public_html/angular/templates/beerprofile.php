567d5e5609e7eb1be6922ba5a8d712f5f4e5bf33
Arlene Graham <agraham14@cnm.edu>
2016-06-15 12:53:25 -0600

    changed 'Rating' to 'Pint Rating' https://app.asana.com/0/117435667927162/118836678397143

--- public_html/angular/templates/beerprofile.php
+++ public_html/angular/templates/beerprofile.php
@@ -22,7 +22,7 @@
 								<ul class="list-group reviews">
 									<li ng-repeat="review in reviewData">
 <!--										Date:  {{ review.reviewDate }}-->
-										Pint Rating: {{ review.reviewPintRating }}<br><br>
+										Pint Rating: {{ review.reviewPintRating }}<br>
 										Review: {{ review.reviewText }}<br><br>
 									</li>
 								</ul>
